6decb8d066c13ed57a8337633f05725c14f2605f
Freddy Kristiansen <freddyk@microsoft.com>
2021-10-11 09:45:35 +0200

    Allow RunNumberOffset for versioning

--- Actions/AL-Go-Helper.ps1
+++ Actions/AL-Go-Helper.ps1
@@ -281,6 +281,7 @@ function ReadSettings {
         "repoVersion"                            = "1.0"
         "repoName"                               = $repoName
         "versioningStrategy"                     = 0
+        "runNumberOffset"                        = 0
         "appBuild"                               = 0
         "appRevision"                            = 0
         "keyVaultName"                           = ""
