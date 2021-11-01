fef1cfdbafd7803751a663c3ceb44efc7482d78e
Freddy Kristiansen <freddyk@microsoft.com>
2021-11-01 10:54:18 +0100

    fix bug

--- Tests/runtests.ps1
+++ Tests/runtests.ps1
@@ -5,7 +5,7 @@ try {
   Set-StrictMode -version 2.0
   $result = Invoke-Pester @(Get-ChildItem -Path (Join-Path $PSScriptRoot "*.Test.ps1")) -passthru
   if ($result.FailedCount -gt 0) {
-    Write-Host "::Error::$($TestResults.FailedCount) tests are failing"
+    Write-Host "::Error::$($result.FailedCount) tests are failing"
     $host.SetShouldExit(1)
   }
 }
