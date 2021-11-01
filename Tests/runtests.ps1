fbaf4c8e4f85f9ee4323edd1f0705d1e899b43ad
Freddy Kristiansen <freddyk@microsoft.com>
2021-11-01 10:52:43 +0100

    use script

--- Tests/runtests.ps1
+++ Tests/runtests.ps1
@@ -1,4 +1,16 @@
 ﻿# PSScriptAnalyzer
 # Run tests
-Invoke-Pester @(Get-ChildItem -Path (Join-Path $PSScriptRoot "*.Test.ps1"))
+try {
+  $errorActionPreference = "stop"
+  Set-StrictMode -version 2.0
+  $result = Invoke-Pester @(Get-ChildItem -Path (Join-Path $PSScriptRoot "*.Test.ps1")) -passthru
+  if ($result.FailedCount -gt 0) {
+    Write-Host "::Error::$($TestResults.FailedCount) tests are failing"
+    $host.SetShouldExit(1)
+  }
+}
+catch {
+  Write-Host "::Error::Error when running tests. The Error was $($_.Exception.Message)"
+  $host.SetShouldExit(1)
+}
 
