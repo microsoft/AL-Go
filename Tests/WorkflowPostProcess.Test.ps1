ec1e8334ccf483e178adc32be8409a639628d0ad
Freddy Kristiansen <freddyk@microsoft.com>
2021-11-18 00:36:29 +0100

    add tests for workflow actions

---
+++ Tests/WorkflowPostProcess.Test.ps1
@@ -0,0 +1,29 @@
+﻿Get-Module TestActionsHelper | Remove-Module -Force
+Import-Module (Join-Path $PSScriptRoot 'TestActionsHelper.psm1')
+
+Describe "WorkflowPostProcess Action Tests" {
+    BeforeAll {
+        $actionName = "WorkflowPostProcess"
+        $scriptRoot = Join-Path $PSScriptRoot "..\Actions\$actionName" -Resolve
+        $scriptName = "$actionName.ps1"
+        $scriptPath = Join-Path $scriptRoot $scriptName
+        $actionScript = GetActionScript -scriptRoot $scriptRoot -scriptName $scriptName
+    }
+
+    It 'Compile Action' {
+        Invoke-Expression $actionScript
+    }
+
+    It 'Test action.yaml matches script' {
+        $permissions = [ordered]@{
+            "contents" = "write"
+            "pull-requests" = "write"
+        }
+        $outputs = [ordered]@{
+        }
+        YamlTest -scriptRoot $scriptRoot -actionName $actionName -actionScript $actionScript -permissions $permissions -outputs $outputs
+    }
+
+    # Call action
+
+}
