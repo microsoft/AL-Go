5eb07f9a2163dc6b54c18fadb1ff4d2b5248904f
Freddy Kristiansen <freddyk@microsoft.com>
2022-02-09 13:41:14 +0100

    Issue #63 - Add CacheKeepDays

--- Actions/RunPipeline/RunPipeline.ps1
+++ Actions/RunPipeline/RunPipeline.ps1
@@ -127,7 +127,7 @@ try {
     $imageName = ""
     if ($repo.gitHubRunner -ne "windows-latest") {
         $imageName = $repo.cacheImageName
-        Flush-ContainerHelperCache -keepdays 3
+        Flush-ContainerHelperCache -keepdays $repo.cacheKeepDays
     }
     $authContext = $null
     $environmentName = ""
