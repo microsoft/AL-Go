098d096f8a782fbd3103f242a3b1cf6177f2d565
freddydk <freddyk@microsoft.com>
2022-12-20 10:57:23 +0100

    dumps

--- Internal/Deploy.ps1
+++ Internal/Deploy.ps1
@@ -294,8 +294,12 @@ try {
         
             Get-ChildItem -Path (Join-Path $srcPath "*") -Recurse | Where-Object { !$_.PSIsContainer } | ForEach-Object {
                 $srcFile = $_.FullName
+                Write-Host "srcFile: $srcFile"
                 $dstFile = $dstPath + $srcFile.Substring($srcPath.Length)
+                Write-Host "dstFile: $dstFile"
                 $dstFilePath = [System.IO.Path]::GetDirectoryName($dstFile)
+                Write-Host "dstFilePath: $dstFilePath"
+
                 if (!(Test-Path $dstFilePath -PathType Container)) {
                     New-Item $dstFilePath -ItemType Directory | Out-Null
                 }
