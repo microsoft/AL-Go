07dd5d0b52ef6251bf9103a9882b5ac3d3162743
freddydk <freddyk@microsoft.com>
2022-12-22 09:25:28 +0100

    exit0

--- e2eTests/RemoveRepositories.ps1
+++ e2eTests/RemoveRepositories.ps1
@@ -16,8 +16,13 @@ Import-Module (Join-Path $PSScriptRoot "e2eTestHelper.psm1") -DisableNameCheckin
 
 SetTokenAndRepository  -github:$github -githubOwner $githubOwner -token $token -repository ''
 
-RemoveRepository -repository "$githubOwner/$actionsRepo"
-RemoveRepository -repository "$githubOwner/$perTenantExtensionRepo"
-RemoveRepository -repository "$githubOwner/$AppSourceAppRepo"
+Write-Host $LASTEXITCODE
 
+RemoveRepository -repository "$githubOwner/$actionsRepo"
 Write-Host "Done $LASTEXITCODE"
+RemoveRepository -repository "$githubOwner/$perTenantExtensionRepo"
+Write-Host "Done $LASTEXITCODE"
+RemoveRepository -repository "$githubOwner/$AppSourceAppRepo"
+Write-Host "Done $LASTEXITCODE"
+
+EXIT 0 # This is needed to make sure the script exits with 0, otherwise the pipeline might fail
