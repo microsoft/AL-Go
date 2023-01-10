8f69f503bd7290291e9da85cbfcc103ef3d745c7
freddydk <freddyk@microsoft.com>
2023-01-10 22:43:05 +0100

    fix projectsettings

--- Actions/Deliver/Deliver.ps1
+++ Actions/Deliver/Deliver.ps1
@@ -185,7 +185,7 @@ try {
         if (Test-Path $customScript -PathType Leaf) {
             Write-Host "Found custom script $customScript for delivery target $deliveryTarget"
             
-            $projectSettings = Get-Content -Path (Join-Path $ENV:GITHUB_WORKSPACE "$thisProject/.AL-Go/settings.json") | ConvertFrom-Json | ConvertTo-HashTable -Recurse
+            $projectSettings = AnalyzeRepo -settings $settings -baseFolder $ENV:GITHUB_WORKSPACE -project $thisProject -doNotCheckArtifactSetting -doNotIssueWarnings
             $parameters = @{
                 "Project" = $thisProject
                 "ProjectName" = $projectName
