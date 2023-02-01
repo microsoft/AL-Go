d2813f9eee00809ef2eb1cb2b43cf60685f60f0d
Alexander Holstrup <aholstrup@microsoft.com>
2023-02-01 09:00:05 +0100

    Update tests with scriptpath

--- Tests/VerifyPRChanges.Action.Test.ps1
+++ Tests/VerifyPRChanges.Action.Test.ps1
@@ -1,11 +1,29 @@
 Describe 'VerifyPRChanges Action Tests' {
 
+    BeforeAll {
+        $scriptPath = Join-Path $PSScriptRoot "..\Actions\VerifyPRChanges\VerifyPRChanges.ps1" -Resolve
+    }
+
     It 'should fail if the PR is from a fork and changes a script' {
         Mock -CommandName Invoke-WebRequest -MockWith { 
             '{ "files": [{ "filename": "Actions/AL-Go-Helper.ps1", "status": "modified" }] }'
         }
        { 
-        C:\Users\aholstrup\Documents\Github\aholstrup\AL-Go\Actions\VerifyPRChanges\VerifyPRChanges.ps1 `
+        & $scriptPath `
+                -baseSHA "123" `
+                -headSHA "456" `
+                -prBaseRepository "microsoft/AL-Go" `
+                -prHeadRepository "fork/AL-Go" `
+                -githubApiUrl "https://api.github.com" 
+        } | Should Throw
+    }
+
+    It 'should fail if the PR is from a fork and changes the CODEOWNERS file' {
+        Mock -CommandName Invoke-WebRequest -MockWith { 
+            '{ "files": [{ "filename": "CODEOWNERS", "status": "modified" }] }'
+        }
+       { 
+        & $scriptPath `
                 -baseSHA "123" `
                 -headSHA "456" `
                 -prBaseRepository "microsoft/AL-Go" `
@@ -19,7 +37,7 @@ Describe 'VerifyPRChanges Action Tests' {
             '{ "files": [{ "filename": "ALModule/Test.Codeunit.al", "status": "modified" }] }'
         }
         Mock Write-Host {}
-        C:\Users\aholstrup\Documents\Github\aholstrup\AL-Go\Actions\VerifyPRChanges\VerifyPRChanges.ps1 `
+        & $scriptPath `
                 -baseSHA "123" `
                 -headSHA "456" `
                 -prBaseRepository "microsoft/AL-Go" `
@@ -30,7 +48,12 @@ Describe 'VerifyPRChanges Action Tests' {
 
     It "should always succeed if it is coming from the same repository" {
         Mock Write-Host {}
-        C:\Users\aholstrup\Documents\Github\aholstrup\AL-Go\Actions\VerifyPRChanges\VerifyPRChanges.ps1 -baseSHA "123" -headSHA "456" -prHeadRepository "microsoft/AL-Go" -prBaseRepository "microsoft/AL-Go" -githubApiUrl "https://api.github.com"
+        & $scriptPath `
+                -baseSHA "123" `
+                -headSHA "456" `
+                -prHeadRepository "microsoft/AL-Go" `
+                -prBaseRepository "microsoft/AL-Go" `
+                -githubApiUrl "https://api.github.com"
         Assert-MockCalled Write-Host -Exactly 1 -Scope It -ParameterFilter { $Object -eq "Pull Request is from the same repository, skipping check." }
     }
 }
