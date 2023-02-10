9ed553a37ded7ecdda4dd4a875d0d34e545a306d
Alexander Holstrup <aholstrup@microsoft.com>
2023-02-10 09:27:59 +0100

    Reorder token in action

--- Actions/VerifyPRChanges/VerifyPRChanges.ps1
+++ Actions/VerifyPRChanges/VerifyPRChanges.ps1
@@ -1,4 +1,6 @@
 Param(
+  [Parameter(HelpMessage = "The token to use for the GitHub API", Mandatory = $false)]
+  [string] $token,
   [Parameter(HelpMessage = "Base commit of the PR", Mandatory = $false)]
   [string] $baseSHA,
   [Parameter(HelpMessage = "Head commit of the PR", Mandatory = $false)]
@@ -6,9 +8,7 @@ Param(
   [Parameter(HelpMessage = "The name of the repository the PR is going to", Mandatory = $false)]
   [string] $prBaseRepository,
   [Parameter(HelpMessage = "The URL of the GitHub API", Mandatory = $false)]
-  [string] $githubApiUrl,
-  [Parameter(HelpMessage = "The token to use for the GitHub API", Mandatory = $false)]
-  [string] $token
+  [string] $githubApiUrl
 
 )
 
