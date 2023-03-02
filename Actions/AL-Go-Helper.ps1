c20c7410e07d7d88f2944b54b84cb0fbbc695739
Alexander Holstrup <aholstrup@microsoft.com>
2023-03-02 11:25:19 +0100

    Logging

--- Actions/AL-Go-Helper.ps1
+++ Actions/AL-Go-Helper.ps1
@@ -734,7 +734,7 @@ function AnalyzeRepo {
             # Check if there are any folders matching $folder
             # Test-Path $folder -PathType Container will return false if any files matches $folder (beside folders)
             if (-not ((Test-Path $folder) -and (Get-ChildItem $folder -Directory))) {
-                if (!$doNotIssueWarnings) { OutputWarning -message "$descr $folderName, specified in $ALGoSettingsFile, does not exist" }
+                if (!$doNotIssueWarnings) { OutputWarning -message "$descr $folderName, specified in $ALGoSettingsFile, does not exist ($folder)" }
             }
             elseif (-not (Test-Path $appJsonFile -PathType Leaf)) {
                 if (!$doNotIssueWarnings) { OutputWarning -message "$descr $folderName, specified in $ALGoSettingsFile, does not contain the source code for an app (no app.json file)" }
