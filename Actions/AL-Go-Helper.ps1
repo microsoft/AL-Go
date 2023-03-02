ae7fbebbe8b3140e573d5922bd45c26ebf9100f2
Alexander Holstrup <117829001+aholstrup1@users.noreply.github.com>
2023-03-02 12:24:27 +0100

    Update AL-Go-Helper.ps1

--- Actions/AL-Go-Helper.ps1
+++ Actions/AL-Go-Helper.ps1
@@ -733,7 +733,7 @@ function AnalyzeRepo {
 
             # Check if there are any folders matching $folder
             # Test-Path $folder -PathType Container will return false if any files matches $folder (beside folders)
-            if (-not (Test-Path $folder -PathType Container)) {
+            if (-not ((Test-Path $folder) -and (Get-ChildItem $folder))) {
                 if (!$doNotIssueWarnings) { OutputWarning -message "$descr $folderName, specified in $ALGoSettingsFile, does not exist" }
             }
             elseif (-not (Test-Path $appJsonFile -PathType Leaf)) {
