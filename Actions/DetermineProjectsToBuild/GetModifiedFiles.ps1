11dd4ca3fdea364e733159c227055de2ecce2e87
mazhelez <mazhelez@microsoft.com>
2023-03-03 13:11:04 +0100

    Inline GetModifiedFiles

--- Actions/DetermineProjectsToBuild/GetModifiedFiles.ps1
+++ Actions/DetermineProjectsToBuild/GetModifiedFiles.ps1
@@ -3,41 +3,27 @@ Param(
     [string] $token
 )
 
-function Get-ModifiedFiles($token) {
-    if(!$env:GITHUB_EVENT_PATH) {
-        Write-Host "GITHUB_EVENT_PATH not set, returning empty list of changed files"
-        return @()
-    }
-    
-    $ghEvent = Get-Content $env:GITHUB_EVENT_PATH -Encoding UTF8 | ConvertFrom-Json
-
-    if(!$ghEvent.pull_request) {
-        Write-Host "Not a pull request, returning empty list of changed files"
-        return @()
-    }
-    
-    $url = "$($env:GITHUB_API_URL)/repos/$($env:GITHUB_REPOSITORY)/compare/$($ghEvent.pull_request.base.sha)...$($ghEvent.pull_request.head.sha)"
-    
-    $headers = @{             
-        "Authorization" = "token $token"
-        "Accept" = "application/vnd.github.baptiste-preview+json"
-    }
-    $response = InvokeWebRequest -Headers $headers -Uri $url | ConvertFrom-Json
-    $filesChanged = @($response.files | ForEach-Object { $_.filename })
-
-    return $filesChanged
+if(!$env:GITHUB_EVENT_PATH) {
+    Write-Host "GITHUB_EVENT_PATH not set, returning empty list of changed files"
+    return @()
 }
 
-$modifiedFiles = Get-ModifiedFiles -token $token
+$ghEvent = Get-Content $env:GITHUB_EVENT_PATH -Encoding UTF8 | ConvertFrom-Json
 
-# If no files were modified, we still need to set the output variable
-if(!$modifiedFiles) {
-    $modifiedFiles = @()
+if(!$ghEvent.pull_request) {
+    Write-Host "Not a pull request, returning empty list of changed files"
+    return @()
 }
 
-$modifiedFilesJson = ConvertTo-Json $modifiedFiles -Depth 99 -Compress
-    
-# Set output variables
-Add-Content -Path $env:GITHUB_OUTPUT -Value "modifiedFiles=$modifiedFilesJson"
+$url = "$($env:GITHUB_API_URL)/repos/$($env:GITHUB_REPOSITORY)/compare/$($ghEvent.pull_request.base.sha)...$($ghEvent.pull_request.head.sha)"
 
-Write-Host "modifiedFiles=$modifiedFilesJson"
+$headers = @{             
+    "Authorization" = "token $token"
+    "Accept" = "application/vnd.github.baptiste-preview+json"
+}
+
+$response = InvokeWebRequest -Headers $headers -Uri $url | ConvertFrom-Json
+
+$filesChanged = @($response.files | ForEach-Object { $_.filename })
+
+return $filesChanged
