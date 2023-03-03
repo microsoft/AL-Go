011a21d939fe993d6d8375247c485a6e7738f583
mazhelez <mazhelez@microsoft.com>
2023-03-03 13:05:29 +0100

    Fix YAML test (v2)

--- Tests/CheckForUpdates.Action.Test.ps1
+++ Tests/CheckForUpdates.Action.Test.ps1
@@ -58,7 +58,7 @@ Describe "CheckForUpdates Action Tests" {
         
         # Locate CheckForUpdates
         $jobsYaml.Find('CheckForUpdates:', [ref] $start, [ref] $count) | Should -be $true
-        $start | Should -be 27
+        $start | Should -be 26
         $count | Should -be 21
         
         # Replace all occurances of 'shell: powershell' with 'shell: pwsh'
@@ -68,7 +68,7 @@ Describe "CheckForUpdates Action Tests" {
         # Replace Permissions
         $yaml.Replace('Permissions:/',@('contents: write','actions: read'))
         $yaml.content[44].Trim() | Should -be 'shell: pwsh'
-        $yaml.content.Count | Should -be 77
+        $yaml.content.Count | Should -be 76
         
         # Get Jobs section (without the jobs: line)
         $jobsYaml = $yaml.Get('jobs:/')
