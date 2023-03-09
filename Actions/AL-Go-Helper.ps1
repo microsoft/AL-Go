5c239e92b8ba1768f832e68be1bae9aff565c3cf
Alexander Holstrup <117829001+aholstrup1@users.noreply.github.com>
2023-03-09 14:32:07 +0100

    Apps without subfolders return false warnings (#430)

--- Actions/AL-Go-Helper.ps1
+++ Actions/AL-Go-Helper.ps1
@@ -732,8 +732,7 @@ function AnalyzeRepo {
             $enumerate = $true
 
             # Check if there are any folders matching $folder
-            # Test-Path $folder -PathType Container will return false if any files matches $folder (beside folders)
-            if (-not ((Test-Path $folder) -and (Get-ChildItem $folder -Directory))) {
+            if (Get-Item $folder | Where-Object { $_ -is [System.IO.DirectoryInfo] }) {
                 if (!$doNotIssueWarnings) { OutputWarning -message "$descr $folderName, specified in $ALGoSettingsFile, does not exist" }
             }
             elseif (-not (Test-Path $appJsonFile -PathType Leaf)) {
