573f329a060b91eb73c9d10246e81bb9e983ffb1
Alexander Holstrup <117829001+aholstrup1@users.noreply.github.com>
2023-03-30 09:57:53 +0200

    Update Deploy.ps1

--- Internal/Deploy.ps1
+++ Internal/Deploy.ps1
@@ -302,10 +302,12 @@ try {
                 $srcFile = $_.FullName
                 $dstFile = $dstPath + $srcFile.Substring($srcPath.Length)
                 $dstFilePath = [System.IO.Path]::GetDirectoryName($dstFile)
+                Write-Host "1"
 
                 if (!(Test-Path $dstFilePath -PathType Container)) {
                     New-Item $dstFilePath -ItemType Directory | Out-Null
                 }
+                Write-Host "2"
                 $lines = ([string](Get-ContentLF -path $srcFile)).Split("`n")
                 "actionsRepo","perTenantExtensionRepo","appSourceAppRepo" | ForEach-Object {
                     if ($_ -eq "actionsRepo") {
@@ -314,10 +316,12 @@ try {
                     else {
                         $useBranch = $branch
                     }
+                    Write-Host "3"
                     $regex = "^(.*)$($originalOwnerAndRepo."$_")(.*)$originalBranch(.*)$"
                     $replace = "`$1$($config.githubOwner)/$($config."$_")`$2$($useBranch)`$3"
                     $lines = $lines | ForEach-Object { $_ -replace $regex, $replace }
                 }
+                Write-Host "4"
                 if ($_.Name -eq "AL-Go-Helper.ps1" -and ($config.PSObject.Properties.Name -eq "defaultBcContainerHelperVersion") -and ($config.defaultBcContainerHelperVersion)) {
                     $lines = $lines | ForEach-Object { $_ -replace '^(\s*)\$defaultBcContainerHelperVersion(\s*)=(\s*)""(.*)$', "`$1`$defaultBcContainerHelperVersion`$2=`$3""$($config.defaultBcContainerHelperVersion)""`$4" }
                 }
@@ -326,6 +330,8 @@ try {
             if (Test-Path -Path (Join-Path '.' '.github') -PathType Container) {
                 Copy-Item -Path (Join-Path $baseRepoPath "RELEASENOTES.md") -Destination (Join-Path "./.github" "RELEASENOTES.copy.md") -Force
             }
+            
+            Write-Host "5"
             
             invoke-git add .
             invoke-git commit --allow-empty -m 'checkout'
