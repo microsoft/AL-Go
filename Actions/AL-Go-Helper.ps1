92c91f00ceffa12a55128eb2e0e824ce6718b9c1
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2023-04-30 14:58:43 +0200

    add recurse

--- Actions/AL-Go-Helper.ps1
+++ Actions/AL-Go-Helper.ps1
@@ -106,7 +106,7 @@ function ConvertTo-HashTable() {
     elseif ($object -is [PSCustomObject]) {
         $object.PSObject.Properties | ForEach-Object {
             if ($recurse -and ($object."$_" -is [System.Collections.Specialized.OrderedDictionary] -or $object."$_" -is [hashtable] -or $_.Value -is [PSCustomObject])) {
-                $ht[$_.Name] = ConvertTo-HashTable $_.Value
+                $ht[$_.Name] = ConvertTo-HashTable $_.Value -recurse
             }
             else {
                 $ht[$_.Name] = $_.Value
