3d2583db3cf99d56246a62d535f8be12b50babb4
Alexander Holstrup <aholstrup@microsoft.com>
2023-05-01 15:15:18 +0200

    Revert to repo

--- Actions/RunPipeline/RunPipeline.ps1
+++ Actions/RunPipeline/RunPipeline.ps1
@@ -169,8 +169,8 @@ try {
     # Analyze InstallApps and InstallTestApps before launching pipeline
 
     # Check if insidersastoken is used (and defined)
-    Write-Host "Code sign certificate secret name: $($settings.codeSignCertificateSecretName)"
-    if (!$repo.doNotSignApps -and $codeSignCertificateUrl -and $codeSignCertificatePassword -and !$settings.codeSignCertificateSecretName) {
+    Write-Host "Code sign certificate secret name: $($repo.codeSignCertificateSecretName)"
+    if (!$repo.doNotSignApps -and $codeSignCertificateUrl -and $codeSignCertificatePassword -and !$repo.codeSignCertificateSecretName) {
         OutputWarning -message "Using the legacy CodeSignCertificateUrl and CodeSignCertificatePassword parameters. Consider using the new Azure Keyvault signing instead."
         $runAlPipelineParams += @{ 
             "CodeSignCertPfxFile" = $codeSignCertificateUrl
