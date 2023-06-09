1ac2c86607e4e3e8bb3209759f63a39912640f07
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2023-06-09 13:52:51 +0200

    add eol

--- e2eTests/Test-AL-Go.ps1
+++ e2eTests/Test-AL-Go.ps1
@@ -105,7 +105,7 @@ SetTokenAndRepository -github:$github -githubOwner $githubOwner -token $token -r
 # Create repo
 # Set DoNotPublishApps to true until we have test apps and set useCompilerFolder
 # This causes the CI/CD workflow to use FilesOnly containers or CompilerFolder (if UseCompilerFolder is true)
-CreateAlGoRepository -github:$github -template $template -branch $branch -private:$private -linux:$linux -addRepoSettings @{ "useCompilerFolder" = $useCompilerFolder.IsPresent; "doNotPublishApps" = $useCompilerFolder.IsPresent } `
+CreateAlGoRepository -github:$github -template $template -branch $branch -private:$private -linux:$linux -addRepoSettings @{ "useCompilerFolder" = $useCompilerFolder.IsPresent; "doNotPublishApps" = $useCompilerFolder.IsPresent }
 $repoPath = (Get-Location).Path
 
 # Get initial number of runs (due to bug in GitHub, this might be 0, 1 or 2)
