049c78cfe97f8854bfac907df40ea5514a532bd5
aholstrup1 <aholstrup@microsoft.com>
2023-07-19 20:23:53 +0200

    git diff from ref

--- Actions/VerifyPRChanges/VerifyPRChanges.ps1
+++ Actions/VerifyPRChanges/VerifyPRChanges.ps1
@@ -3,6 +3,7 @@ Param(
   [string] $token,
   [Parameter(HelpMessage = "Base commit of the PR", Mandatory = $false)]
   [string] $baseSHA,
+  [string] $baseRef,
   [Parameter(HelpMessage = "Head commit of the PR", Mandatory = $false)]
   [string] $headSHA,
   [Parameter(HelpMessage = "The name of the repository the PR is going to", Mandatory = $false)]
@@ -11,7 +12,6 @@ Param(
 
 $ErrorActionPreference = "STOP"
 Set-StrictMode -version 2.0
-git fetch 
-$diff = git diff $baseSHA..$headSHA --name-only
+$diff = git diff HEAD..origin/$baseRef --name-only
 
 Write-Host $diff
