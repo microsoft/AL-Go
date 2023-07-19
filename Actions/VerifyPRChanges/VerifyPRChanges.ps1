53bc8953476d9e70fa8132637dce9f59bb5b7969
aholstrup1 <aholstrup@microsoft.com>
2023-07-19 19:54:21 +0200

    run git diff

--- Actions/VerifyPRChanges/VerifyPRChanges.ps1
+++ Actions/VerifyPRChanges/VerifyPRChanges.ps1
@@ -11,20 +11,7 @@ Param(
 
 $ErrorActionPreference = "STOP"
 Set-StrictMode -version 2.0
-$headers = @{             
-  "Authorization" = "token $token"
-  "Accept"        = "application/vnd.github.baptiste-preview+json"
-}
-$url = "https://api.github.com/repos/$($prBaseRepository)/compare/$baseSHA...$headSHA"
-$response = Invoke-WebRequest -UseBasicParsing -Headers $headers -Uri $url | ConvertFrom-Json
-Write-Host "Files Changed:"
-$response.files | ForEach-Object {
-  $filename = $_.filename
-  Write-Host "- $filename $_.status"
-  $extension = [System.IO.Path]::GetExtension($filename)
-  $name = [System.IO.Path]::GetFileName($filename)
-  if ($extension -eq '.ps1' -or $extension -eq '.yaml' -or $extension -eq '.yml' -or $name -eq "CODEOWNERS" -or $filename.StartsWith(".github/")) {
-    throw "Pull Request containing changes to scripts, workflows or CODEOWNERS are not allowed from forks."
-  }
-}
-Write-Host "Verification completed successfully."
+
+$diff = git diff $baseSHA..$headSHA --name-only
+
+Write-Host $diff
