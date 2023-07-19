e0fddc5488e08e0710458bc0de8dca942727ba72
freddydk <freddydk@users.noreply.github.com>
2023-07-20 00:27:24 +0200

    bcpt

--- Actions/AnalyzeTests/AnalyzeTests.ps1
+++ Actions/AnalyzeTests/AnalyzeTests.ps1
@@ -26,26 +26,33 @@ try {
     . (Join-Path -Path $PSScriptRoot 'TestResultAnalyzer.ps1')
 
     $testResultsFile = Join-Path $ENV:GITHUB_WORKSPACE "$project\TestResults.xml"
-    if (Test-Path $testResultsFile) {
-        $testResults = [xml](Get-Content "$project\TestResults.xml")
-        $testResultSummary = GetTestResultSummary -testResults $testResults -includeFailures 50
-
-        Add-Content -Path $env:GITHUB_OUTPUT -Value "TestResultMD=$testResultSummary"
-        Write-Host "TestResultMD=$testResultSummary"
-    
-        Add-Content -path $ENV:GITHUB_STEP_SUMMARY -value "$($testResultSummary.Replace("\n","`n"))`n"
-    }
-    else {
-        Add-Content -path $ENV:GITHUB_STEP_SUMMARY -value "*Test results not found*`n`n"
-    }
+    $testResultsSummaryMD, $testResultsfailuresMD, $failuresSummaryMD = GetTestResultSummaryMD -path $testResultsFile
 
     $bcptTestResultsFile = Join-Path $ENV:GITHUB_WORKSPACE "$project\BCPTTestResults.json"
-    if (Test-Path $bcptTestResultsFile) {
-        Add-Content -path $ENV:GITHUB_STEP_SUMMARY -value "## BCPT test results`n`n"
+    $bcptBaseLineFile = Join-Path $ENV:GITHUB_WORKSPACE "$project\BCPTBaseLine.json"
+    $bcptSummaryMD = GetBcptSummaryMD -path $bcptTestResultsFile -baseLinePath $bcptBaseLineFile
+
+    # If summary fits, we will display it in the GitHub summary
+    if ($summaryMD.Length -gt 65000) {
+        # If Test results summary is too long, we will not display it in the GitHub summary, instead we will display a message to download the test results
+        $summaryMD = "<i>Test results summary is too long to be displayed in the GitHub summary. Download test results to see details.</i>"
     }
-    else {
-        Add-Content -path $ENV:GITHUB_STEP_SUMMARY -value "*BCPT test results not found*`n`n"
+    # If summary AND BCPT summary fits, we will display both in the GitHub summary
+    if ($summaryMD.Length+$bcptSummaryMD.Length -gt 65000) {
+        # If Combined Test Results and BCPT summary exceeds GitHub summary capacity, we will not display the BCPT summary
+        $bcptSummaryMD = "<i>Performance test results summary is too long to be displayed in the GitHub summary. Download BCPT Test results to see details.</i>"
     }
+    # If summary AND BCPT summary AND failures summary fits, we will display all in the GitHub summary
+    if ($summaryMD.Length+$failuresMD.Length+$bcptSummaryMD.Length -gt 65000) {
+        # If Combined Test Results, failures and BCPT summary exceeds GitHub summary capacity, we will not display the failures details, only the failures summary
+        $failuresMD = $failuresSummaryMD
+    }
+
+    Add-Content -path $ENV:GITHUB_STEP_SUMMARY -value "## Test results`n`n"
+    Add-Content -path $ENV:GITHUB_STEP_SUMMARY -value "$($summaryMD.Replace("\n","`n"))`n`n"
+    Add-Content -path $ENV:GITHUB_STEP_SUMMARY -value "$($failuresMD.Replace("\n","`n"))`n`n"
+    Add-Content -path $ENV:GITHUB_STEP_SUMMARY -value "## Performance test results`n`n"
+    Add-Content -path $ENV:GITHUB_STEP_SUMMARY -value "$($bcptSummaryMD.Replace("\n","`n"))`n`n"
 
     TrackTrace -telemetryScope $telemetryScope
 }
--- Actions/AnalyzeTests/TestResultAnalyzer.ps1
+++ Actions/AnalyzeTests/TestResultAnalyzer.ps1
@@ -1,108 +1,125 @@
-function GetTestResultSummary {
+function ReadBcptFile {
     Param(
-        [xml] $testResults,
-        [int] $includeFailures
+        [string] $path
     )
 
-    $totalTests = 0
-    $totalTime = 0.0
-    $totalFailed = 0
-    $totalSkipped = 0
-    $failuresIncluded = 0
+
+}
+
+
+function GetBcptSummaryMD {
+    Param(
+        [string] $path,
+        [string] $baseLinePath
+    )
+
+    $summarySb = [System.Text.StringBuilder]::new()
+
+
+    $summarySb.ToString()
+}
+
+# Build MarkDown of TestResults file
+# This function will not fail if the file does not exist or if any test errors are found
+# TestResults is in JUnit format
+# Returns both a summary part and a failures part
+function GetTestResultSummaryMD {
+    Param(
+        [string] $path
+    )
+
     $summarySb = [System.Text.StringBuilder]::new()
     $failuresSb = [System.Text.StringBuilder]::new()
-    if ($testResults.testsuites) {
-        $appNames = @($testResults.testsuites.testsuite | ForEach-Object { $_.Properties.property | Where-Object { $_.Name -eq "appName" } | ForEach-Object { $_.Value } } | Select-Object -Unique)
-        if (-not $appNames) {
-            $appNames = @($testResults.testsuites.testsuite | ForEach-Object { $_.Properties.property | Where-Object { $_.Name -eq "extensionId" } | ForEach-Object { $_.Value } } | Select-Object -Unique)
-        }
-        $testResults.testsuites.testsuite | ForEach-Object {
-            $totalTests += $_.Tests
-            $totalTime += [decimal]::Parse($_.time, [System.Globalization.CultureInfo]::InvariantCulture)
-            $totalFailed += $_.failures
-            $totalSkipped += $_.skipped
-        }
-        Write-Host "$($appNames.Count) TestApps, $totalTests tests, $totalFailed failed, $totalSkipped skipped, $totalTime seconds"
-        $summarySb.Append('|Test app|Tests|Passed|Failed|Skipped|Time|\n|:---|---:|---:|---:|---:|---:|\n') | Out-Null
-        $appNames | ForEach-Object {
-            $appName = $_
-            $appTests = 0
-            $appTime = 0.0
-            $appFailed = 0
-            $appSkipped = 0
-            $suites = $testResults.testsuites.testsuite | where-Object { $_.Properties.property | Where-Object { $_.Value -eq $appName } }
-            $suites | ForEach-Object {
-                $appTests += [int]$_.tests
-                $appFailed += [int]$_.failures
-                $appSkipped += [int]$_.skipped
-                $appTime += [decimal]::Parse($_.time, [System.Globalization.CultureInfo]::InvariantCulture)
+    if (Test-Path -Path $path -PathType Leaf) {
+        $testResults = [xml](Get-Content "$project\TestResults.xml" -Encoding UTF8)
+        $totalTests = 0
+        $totalTime = 0.0
+        $totalFailed = 0
+        $totalSkipped = 0
+        if ($testResults.testsuites) {
+            $appNames = @($testResults.testsuites.testsuite | ForEach-Object { $_.Properties.property | Where-Object { $_.Name -eq "appName" } | ForEach-Object { $_.Value } } | Select-Object -Unique)
+            if (-not $appNames) {
+                $appNames = @($testResults.testsuites.testsuite | ForEach-Object { $_.Properties.property | Where-Object { $_.Name -eq "extensionId" } | ForEach-Object { $_.Value } } | Select-Object -Unique)
             }
-            $appPassed = $appTests-$appFailed-$appSkipped
-            Write-Host "- $appName, $appTests tests, $appPassed passed, $appFailed failed, $appSkipped skipped, $appTime seconds"
-            $summarySb.Append("|$appName|$appTests|") | Out-Null
-            if ($appPassed -gt 0) {
-                $summarySb.Append("$($appPassed):white_check_mark:") | Out-Null
+            $testResults.testsuites.testsuite | ForEach-Object {
+                $totalTests += $_.Tests
+                $totalTime += [decimal]::Parse($_.time, [System.Globalization.CultureInfo]::InvariantCulture)
+                $totalFailed += $_.failures
+                $totalSkipped += $_.skipped
             }
-            $summarySb.Append("|") | Out-Null
-            if ($appFailed -gt 0) {
-                $summarySb.Append("$($appFailed):x:") | Out-Null
-            }
-            $summarySb.Append("|") | Out-Null
-            if ($appSkipped -gt 0) {
-                $summarySb.Append("$($appSkipped):white_circle:") | Out-Null
-            }
-            $summarySb.Append("|$($appTime)s|\n") | Out-Null
-            if ($appFailed -gt 0) {
-                $failuresSb.Append("<details><summary><i>$appName, $appTests tests, $appPassed passed, $appFailed failed, $appSkipped skipped, $appTime seconds</i></summary>\n") | Out-Null
+            Write-Host "$($appNames.Count) TestApps, $totalTests tests, $totalFailed failed, $totalSkipped skipped, $totalTime seconds"
+            $summarySb.Append('|Test app|Tests|Passed|Failed|Skipped|Time|\n|:---|---:|---:|---:|---:|---:|\n') | Out-Null
+            $appNames | ForEach-Object {
+                $appName = $_
+                $appTests = 0
+                $appTime = 0.0
+                $appFailed = 0
+                $appSkipped = 0
+                $suites = $testResults.testsuites.testsuite | where-Object { $_.Properties.property | Where-Object { $_.Value -eq $appName } }
                 $suites | ForEach-Object {
-                    Write-Host "  - $($_.name), $($_.tests) tests, $($_.failures) failed, $($_.skipped) skipped, $($_.time) seconds"
-                    if ($_.failures -gt 0 -and $failuresSb.Length -lt 32000) {
-                        $failuresSb.Append("<details><summary><i>$($_.name), $($_.tests) tests, $($_.failures) failed, $($_.skipped) skipped, $($_.time) seconds</i></summary>") | Out-Null
-                        $_.testcase | ForEach-Object {
-                            if ($_.ChildNodes.Count -gt 0) {
-                                Write-Host "    - $($_.name), Failure, $($_.time) seconds"
-                                $failuresSb.Append("<details><summary><i>$($_.name), Failure</i></summary>") | Out-Null
-                                $_.ChildNodes | ForEach-Object {
-                                    Write-Host "      - Error: $($_.message)"
-                                    Write-Host "        Stacktrace:"
-                                    Write-Host "        $($_."#text".Trim().Replace("`n","`n        "))"
-                                    $failuresSb.Append("<i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Error: $($_.message)</i><br/>") | Out-Null
-                                    $failuresSb.Append("<i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Stack trace</i><br/>") | Out-Null
-                                    $failuresSb.Append("<i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$($_."#text".Trim().Replace("`n","<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"))</i><br/>") | Out-Null
-                                }
-                                $failuresSb.Append("</details>") | Out-Null
-                            }
-                        }
-                        $failuresSb.Append("</details>") | Out-Null
-                        $failuresIncluded++
-                    }
+                    $appTests += [int]$_.tests
+                    $appFailed += [int]$_.failures
+                    $appSkipped += [int]$_.skipped
+                    $appTime += [decimal]::Parse($_.time, [System.Globalization.CultureInfo]::InvariantCulture)
                 }
-                $failuresSb.Append("</details>") | Out-Null
+                $appPassed = $appTests-$appFailed-$appSkipped
+                Write-Host "- $appName, $appTests tests, $appPassed passed, $appFailed failed, $appSkipped skipped, $appTime seconds"
+                $summarySb.Append("|$appName|$appTests|") | Out-Null
+                if ($appPassed -gt 0) {
+                    $summarySb.Append("$($appPassed):white_check_mark:") | Out-Null
+                }
+                $summarySb.Append("|") | Out-Null
+                if ($appFailed -gt 0) {
+                    $summarySb.Append("$($appFailed):x:") | Out-Null
+                }
+                $summarySb.Append("|") | Out-Null
+                if ($appSkipped -gt 0) {
+                    $summarySb.Append("$($appSkipped):white_circle:") | Out-Null
+                }
+                $summarySb.Append("|$($appTime)s|\n") | Out-Null
+                if ($appFailed -gt 0) {
+                    $failuresSb.Append("<details><summary><i>$appName, $appTests tests, $appPassed passed, $appFailed failed, $appSkipped skipped, $appTime seconds</i></summary>\n") | Out-Null
+                    $suites | ForEach-Object {
+                        Write-Host "  - $($_.name), $($_.tests) tests, $($_.failures) failed, $($_.skipped) skipped, $($_.time) seconds"
+                        if ($_.failures -gt 0 -and $failuresSb.Length -lt 32000) {
+                            $failuresSb.Append("<details><summary><i>$($_.name), $($_.tests) tests, $($_.failures) failed, $($_.skipped) skipped, $($_.time) seconds</i></summary>") | Out-Null
+                            $_.testcase | ForEach-Object {
+                                if ($_.ChildNodes.Count -gt 0) {
+                                    Write-Host "    - $($_.name), Failure, $($_.time) seconds"
+                                    $failuresSb.Append("<details><summary><i>$($_.name), Failure</i></summary>") | Out-Null
+                                    $_.ChildNodes | ForEach-Object {
+                                        Write-Host "      - Error: $($_.message)"
+                                        Write-Host "        Stacktrace:"
+                                        Write-Host "        $($_."#text".Trim().Replace("`n","`n        "))"
+                                        $failuresSb.Append("<i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Error: $($_.message)</i><br/>") | Out-Null
+                                        $failuresSb.Append("<i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Stack trace</i><br/>") | Out-Null
+                                        $failuresSb.Append("<i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$($_."#text".Trim().Replace("`n","<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"))</i><br/>") | Out-Null
+                                    }
+                                    $failuresSb.Append("</details>") | Out-Null
+                                }
+                            }
+                            $failuresSb.Append("</details>") | Out-Null
+                        }
+                    }
+                    $failuresSb.Append("</details>") | Out-Null
+                }
             }
         }
-    }
-    if ($totalFailed -gt 0) {
-        if ($totalFailed -gt $failuresIncluded) {
-            $failuresSb.Insert(0,"<details><summary><i>$totalFailed failing tests (showing the first $failuresIncluded here, download test results to see all)</i></summary>") | Out-Null
+        $failuresSummaryMD = ""
+        if ($totalFailed -gt 0) {
+            $failuresSummaryMD = "<i>$totalFailed failing tests, download test results to see details</i>"
+            $failuresSb.Insert(0,"<details><summary>$failuresSummaryMD</summary>") | Out-Null
+            $failuresSb.Append("</details>") | Out-Null
         }
         else {
-            $failuresSb.Insert(0,"<details><summary><i>$totalFailed failing tests</i></summary>") | Out-Null
-        }
-        $failuresSb.Append("</details>") | Out-Null
-        if (($summarySb.Length + $failuresSb.Length) -lt 65000) {
-            $summarySb.Append("\n\n$($failuresSb.ToString())") | Out-Null
-        }
-        else {
-            $summarySb.Append("\n\n<i>$totalFailed failing tests. Download test results to see all</i>") | Out-Null
+            $failuresSummaryMD = "<i>No test failures</i>"
+            $failuresDb.Append($failuresSummaryMD) | Out-Null
         }
     }
     else {
-        $summarySb.Append("\n\n<i>No test failures</i>") | Out-Null
+        $summarySb.Append("<i>No test results found</i>") | Out-Null
     }
-    if ($summarySb.Length -lt 65500) {
-        $summarySb.ToString()
-    }
-    else {
-        "<i>$totalFailed failing tests. Download test results to see all</i>"
-    }
+    $summarySb.ToString()
+    $failuresSb.ToString()
+    $failuresSummaryMD
 }
