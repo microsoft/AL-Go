46e48254fc51def36777e5577d22626d2014a6a2
Alexander Holstrup <117829001+aholstrup1@users.noreply.github.com>
2023-07-20 17:13:14 +0200

    Update to Verify PR Changes (#612)
    Update to Verify PR Changes

--- Actions/VerifyPRChanges/VerifyPRChanges.ps1
+++ Actions/VerifyPRChanges/VerifyPRChanges.ps1
@@ -6,8 +6,78 @@ Param(
   [Parameter(HelpMessage = "Head commit of the PR", Mandatory = $false)]
   [string] $headSHA,
   [Parameter(HelpMessage = "The name of the repository the PR is going to", Mandatory = $false)]
-  [string] $prBaseRepository
+  [string] $prBaseRepository,
+  [Parameter(HelpMessage = "The id of the pull request", Mandatory = $false)]
+  [string] $pullRequestId
 )
+
+function ValidateFiles
+(
+  [Object[]] $Files
+)
+{
+  $disallowedExtensions = @('.ps1', '.psm1', '.yml', '.yaml')
+  $disallowedFiles = @('CODEOWNERS')
+
+  $Files | ForEach-Object {
+    $filename = $_.filename
+    $status = $_.status
+    Write-Host "- $filename $status"
+    $extension = [System.IO.Path]::GetExtension($filename)
+    $name = [System.IO.Path]::GetFileName($filename)
+    if (($extension -in $disallowedExtensions) -or ($name -in $disallowedFiles) -or $filename.StartsWith(".github/")) {
+      throw "Pull Request containing changes to scripts, workflows or CODEOWNERS are not allowed from forks."
+    }
+  } 
+}
+
+function ValidatePullRequest
+(
+  [string[]] $PullRequestRepository,
+  [string[]] $PullRequestId,
+  [hashtable] $Headers,
+  [int] $MaxAllowedChangedFiles = 3000
+)
+{
+  $url = "https://api.github.com/repos/$($prBaseRepository)/pulls/$pullRequestId"
+  $pullRequestDetails = Invoke-WebRequest -UseBasicParsing -Headers $Headers -Uri $url | ConvertFrom-Json
+
+  # List Pull Request files has a max of 3000 files. https://docs.github.com/en/rest/pulls/pulls?apiVersion=2022-11-28#list-pull-requests-files
+  if ($pullRequestDetails.changed_files -gt $MaxAllowedChangedFiles) {
+    throw "Pull request contains changes to $($pullRequestDetails.changed_files) files. You cannot change more than $MaxAllowedChangedFiles files from a fork."
+  }
+}
+
+function ValidatePullRequestFiles
+(
+  [string[]] $PullRequestRepository,
+  [string[]] $PullRequestId,
+  [hashtable] $Headers
+)
+{
+  $pageNumber = 1
+  $resultsPerPage = 100
+  $hasMoreData = $true
+  Write-Host "Files Changed:"
+  while ($hasMoreData) {
+    $url = "https://api.github.com/repos/$($prBaseRepository)/pulls/$pullRequestId/files?per_page=$resultsPerPage&page=$pageNumber"
+    $changedFiles = Invoke-WebRequest -UseBasicParsing -Headers $headers -Uri $url | ConvertFrom-Json
+
+    # Finish check if there are no more files to be validated
+    if (-not $changedFiles) {
+      break
+    }
+
+    ValidateFiles -Files $changedFiles
+
+    if ($changedFiles -and ($changedFiles.Count -eq $resultsPerPage)) {
+      $pageNumber += 1
+    } else {
+      $hasMoreData = $false
+    }
+  }
+  Write-Host "Verification completed successfully."
+}
 
 $ErrorActionPreference = "STOP"
 Set-StrictMode -version 2.0
@@ -15,16 +85,7 @@ $headers = @{
   "Authorization" = "token $token"
   "Accept"        = "application/vnd.github.baptiste-preview+json"
 }
-$url = "https://api.github.com/repos/$($prBaseRepository)/compare/$baseSHA...$headSHA"
-$response = Invoke-WebRequest -UseBasicParsing -Headers $headers -Uri $url | ConvertFrom-Json
-Write-Host "Files Changed:"
-$response.files | ForEach-Object {
-  $filename = $_.filename
-  Write-Host "- $filename $_.status"
-  $extension = [System.IO.Path]::GetExtension($filename)
-  $name = [System.IO.Path]::GetFileName($filename)
-  if ($extension -eq '.ps1' -or $extension -eq '.yaml' -or $extension -eq '.yml' -or $name -eq "CODEOWNERS" -or $filename.StartsWith(".github/")) {
-    throw "Pull Request containing changes to scripts, workflows or CODEOWNERS are not allowed from forks."
-  }
-}
-Write-Host "Verification completed successfully."
+
+
+ValidatePullRequest -PullRequestRepository $prBaseRepository -PullRequestId $pullRequestId -Headers $headers
+ValidatePullRequestFiles -PullRequestRepository $prBaseRepository -PullRequestId $pullRequestId -Headers $headers
--- Tests/VerifyPRChanges.Action.Test.ps1
+++ Tests/VerifyPRChanges.Action.Test.ps1
@@ -12,107 +12,142 @@ Describe 'VerifyPRChanges Action Tests' {
     }
 
     It 'should fail if the PR is from a fork and changes a script' {
-        Mock -CommandName Invoke-WebRequest -MockWith { 
-            '{ "files": [{ "filename": "Scripts/BuildScript.ps1", "status": "modified" }] }'
-        }
+        Mock -CommandName Invoke-WebRequest -MockWith {  '{ "changed_files": 1 }' } -ParameterFilter { $Uri -and $Uri -notmatch "/files"}
+        Mock -CommandName Invoke-WebRequest -MockWith {  '[{"filename": "Scripts/BuildScript.ps1", "status": "modified"}]' } -ParameterFilter { $Uri -and $Uri -match "/files"}
+
        { 
         & $scriptPath `
                 -baseSHA "123" `
                 -headSHA "456" `
                 -prBaseRepository "microsoft/AL-Go" `
+                -pullRequestId "123456" `
                 -token "ABC" 
         } | Should -Throw
     }
 
     It 'should fail if the PR is from a fork and adds a script' {
-        Mock -CommandName Invoke-WebRequest -MockWith { 
-            '{ "files": [{ "filename": "Scripts/BuildScript.ps1", "status": "added" }] }'
-        }
+        Mock -CommandName Invoke-WebRequest -MockWith {  '{ "changed_files": 1 }' } -ParameterFilter { $Uri -and $Uri -notmatch "/files"}
+        Mock -CommandName Invoke-WebRequest -MockWith {  '[{"filename":"Scripts/BuildScript.ps1", "status": "added"}]' } -ParameterFilter { $Uri -and $Uri -match "/files"}
+
        { 
         & $scriptPath `
                 -baseSHA "123" `
                 -headSHA "456" `
                 -prBaseRepository "microsoft/AL-Go" `
+                -pullRequestId "123456" `
                 -token "ABC" 
         } | Should -Throw
     }
 
     It 'should fail if the PR is from a fork and removes a script' {
-        Mock -CommandName Invoke-WebRequest -MockWith { 
-            '{ "files": [{ "filename": "Scripts/BuildScript.ps1", "status": "removed" }] }'
-        }
+        Mock -CommandName Invoke-WebRequest -MockWith {  '{ "changed_files": 1 }' } -ParameterFilter { $Uri -and $Uri -notmatch "/files"}
+        Mock -CommandName Invoke-WebRequest -MockWith {  '[{"filename":"Scripts/BuildScript.ps1","status":"removed"}]' } -ParameterFilter { $Uri -and $Uri -match "/files"}
+
        { 
         & $scriptPath `
                 -baseSHA "123" `
                 -headSHA "456" `
                 -prBaseRepository "microsoft/AL-Go" `
+                -pullRequestId "123456" `
                 -token "ABC" 
         } | Should -Throw
     }
 
     It 'should fail if the PR is from a fork and changes the CODEOWNERS file' {
-        Mock -CommandName Invoke-WebRequest -MockWith { 
-            '{ "files": [{ "filename": "CODEOWNERS", "status": "modified" }] }'
-        }
+        Mock -CommandName Invoke-WebRequest -MockWith {  '{ "changed_files": 1 }' } -ParameterFilter { $Uri -and $Uri -notmatch "/files"}
+        Mock -CommandName Invoke-WebRequest -MockWith {  '[{"filename":"CODEOWNERS","status":"modified"}]' } -ParameterFilter { $Uri -and $Uri -match "/files"}
+
        { 
         & $scriptPath `
                 -baseSHA "123" `
                 -headSHA "456" `
                 -prBaseRepository "microsoft/AL-Go" `
+                -pullRequestId "123456" `
                 -token "ABC" 
         } | Should -Throw
     }
 
     It 'should fail if the PR is from a fork and changes anything in the .github folder' {
-        Mock -CommandName Invoke-WebRequest -MockWith { 
-            '{ "files": [{ "filename": ".github/Settings.json", "status": "modified" }] }'
-        }
+        Mock -CommandName Invoke-WebRequest -MockWith {  '{ "changed_files": 1 }' } -ParameterFilter { $Uri -and $Uri -notmatch "/files"}
+        Mock -CommandName Invoke-WebRequest -MockWith {  '[{"filename":".github/Settings.json","status":"modified"}]' } -ParameterFilter { $Uri -and $Uri -match "/files"}
+
        { 
         & $scriptPath `
                 -baseSHA "123" `
                 -headSHA "456" `
                 -prBaseRepository "microsoft/AL-Go" `
+                -pullRequestId "123456" `
+                -token "ABC" 
+        } | Should -Throw
+    }
+
+    It 'should fail if the PR is from a fork and changes a yml file' {
+        Mock -CommandName Invoke-WebRequest -MockWith {  '{ "changed_files": 1 }' } -ParameterFilter { $Uri -and $Uri -notmatch "/files"}
+        Mock -CommandName Invoke-WebRequest -MockWith {  '[{"filename":".github/workflows/test.yaml","status":"modified"}]' } -ParameterFilter { $Uri -and $Uri -match "/files"}
+
+       { 
+        & $scriptPath `
+                -baseSHA "123" `
+                -headSHA "456" `
+                -prBaseRepository "microsoft/AL-Go" `
+                -pullRequestId "123456" `
                 -token "ABC" 
         } | Should -Throw
     }
 
     It 'should succeed if the PR is from a fork and changes an .al file' {
-        Mock -CommandName Invoke-WebRequest -MockWith { 
-            '{ "files": [{ "filename": "ALModule/Test.Codeunit.al", "status": "modified" }] }'
-        }
+        Mock -CommandName Invoke-WebRequest -MockWith {  '{ "changed_files": 1 }' } -ParameterFilter { $Uri -and $Uri -notmatch "/files"}
+        Mock -CommandName Invoke-WebRequest -MockWith {  '[{"filename":"ALModule/Test.Codeunit.al","status":"modified"}]' } -ParameterFilter { $Uri -and $Uri -match "/files"}
+
         Mock Write-Host {}
         & $scriptPath `
                 -baseSHA "123" `
                 -headSHA "456" `
                 -prBaseRepository "microsoft/AL-Go" `
+                -pullRequestId "123456" `
                 -token "ABC" 
         Assert-MockCalled Write-Host -Exactly 1 -Scope It -ParameterFilter { $Object -eq "Verification completed successfully." }
     }
 
     It 'should succeed if the PR is from a fork and adds an .al file' {
-        Mock -CommandName Invoke-WebRequest -MockWith { 
-            '{ "files": [{ "filename": "ALModule/Test.Codeunit.al", "status": "added" }] }'
-        }
+        Mock -CommandName Invoke-WebRequest -MockWith {  '{ "changed_files": 1 }' } -ParameterFilter { $Uri -and $Uri -notmatch "/files"}
+        Mock -CommandName Invoke-WebRequest -MockWith {  '[{"filename":"ALModule/Test.Codeunit.al","status":"added"}]' } -ParameterFilter { $Uri -and $Uri -match "/files"}
+
         Mock Write-Host {}
         & $scriptPath `
                 -baseSHA "123" `
                 -headSHA "456" `
                 -prBaseRepository "microsoft/AL-Go" `
+                -pullRequestId "123456" `
                 -token "ABC" 
         Assert-MockCalled Write-Host -Exactly 1 -Scope It -ParameterFilter { $Object -eq "Verification completed successfully." }
     }
 
     It 'should succeed if the PR is from a fork and removes an .al file' {
-        Mock -CommandName Invoke-WebRequest -MockWith { 
-            '{ "files": [{ "filename": "ALModule/Test.Codeunit.al", "status": "removed" }] }'
-        }
+        Mock -CommandName Invoke-WebRequest -MockWith {  '{ "changed_files": 1 }' } -ParameterFilter { $Uri -and $Uri -notmatch "/files"}
+        Mock -CommandName Invoke-WebRequest -MockWith {  '[{"filename":"ALModule/Test.Codeunit.al","status":"removed"}]' } -ParameterFilter { $Uri -and $Uri -match "/files"}
+
         Mock Write-Host {}
         & $scriptPath `
                 -baseSHA "123" `
                 -headSHA "456" `
                 -prBaseRepository "microsoft/AL-Go" `
+                -pullRequestId "123456" `
                 -token "ABC" 
         Assert-MockCalled Write-Host -Exactly 1 -Scope It -ParameterFilter { $Object -eq "Verification completed successfully." }
+    }
+
+    It 'should fail if the PR is from a fork and changes more than 3000 files' {
+        Mock -CommandName Invoke-WebRequest -MockWith {  '{ "changed_files": 5001 }' } -ParameterFilter { $Uri -and $Uri -notmatch "/files"}
+
+       { 
+        & $scriptPath `
+                -baseSHA "123" `
+                -headSHA "456" `
+                -prBaseRepository "microsoft/AL-Go" `
+                -pullRequestId "123456" `
+                -token "ABC" 
+        } | Should -Throw
     }
 
     It 'Compile Action' {
