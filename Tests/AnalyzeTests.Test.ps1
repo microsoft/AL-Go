7fa0b49da1ac602d14de5696f49e446df3093401
freddydk <freddydk@users.noreply.github.com>
2023-07-26 00:30:48 +0200

    tests

--- Tests/AnalyzeTests.Test.ps1
+++ Tests/AnalyzeTests.Test.ps1
@@ -4,9 +4,11 @@ Import-Module (Join-Path $PSScriptRoot 'TestActionsHelper.psm1')
 function GetBcptTestResultFile {
     Param(
         [int] $noOfSuites = 1,
-        [int] $noOfCodeunits = 100,
-        [int] $noOfOperations = 100,
-        [int] $noOfMeasurements = 100
+        [int] $noOfCodeunits = 1,
+        [int] $noOfOperations = 1,
+        [int] $noOfMeasurements = 1,
+        [int] $durationOffset = 0,
+        [int] $numberOfSQLStmtsOffset = 0
     )
 
     $bcpt = @()
@@ -16,7 +18,8 @@ function GetBcptTestResultFile {
             $codeunitID = $_
             $codeunitName = "Codeunit$_"
             1..$noOfOperations | ForEach-Object {
-                $operationName = "Operation$_"
+                $operationNo = $_
+                $operationName = "Operation$operationNo"
                 1..$noOfMeasurements | ForEach-Object {
                     $no = $_
                     $bcpt += @(@{
@@ -25,15 +28,20 @@ function GetBcptTestResultFile {
                         "codeunitID" = $codeunitID
                         "codeunitName" = $codeunitName
                         "operation" = $operationName
-                        "durationMin" = 1000*$codeunitID+$no
-                        "numberOfSQLStmts" = $codeunitID+$no
+                        "durationMin" = $codeunitNo*100+$operationNo*10+$no+$durationOffset
+                        "numberOfSQLStmts" = $operationNo+$numberOfSQLStmtsOffset
                     })
                 }
             }
         }
     }
-    $bcpt | ConvertTo-Json -Depth 100 | set-content -path c:\temp\bcpt.json -encoding UTF8
+    $filename = Join-Path $ENV:TEMP "$([GUID]::NewGuid().ToString()).json"
+    $bcpt | ConvertTo-Json -Depth 100 | Set-Content -Path $filename -Encoding UTF8
+    return $filename
 }
+
+$bcptFilename = GetBcptTestResultFile -noOfSuites 1 -noOfCodeunits 2 -noOfOperations 3 -noOfMeasurements 4
+$bcptBaseLine = GetBcptTestResultFile -noOfSuites 1 -noOfCodeunits 4 -noOfOperations 6 -noOfMeasurements 4 -durationOffset -1 -numberOfSQLStmtsOffset -1
 
 Describe "AnalyzeTests Action Tests" {
     BeforeAll {
@@ -58,6 +66,27 @@ Describe "AnalyzeTests Action Tests" {
 
     It 'Test ReadBcptFile' {
         . (Join-Path $scriptRoot 'TestResultAnalyzer.ps1')
-        Write-Host $test
+        $bcpt = ReadBcptFile -path $bcptFilename
+        $bcpt.Count | should -Be 1
+        $bcpt."SUITE1".Count | should -Be 2
+        $bcpt."SUITE1"."1".operations.Count | should -Be 3
+        $bcpt."SUITE1"."1".operations."operation2".measurements.Count | should -Be 4
     }
+
+    It 'Test GetBcptSummaryMD (no baseline)' {
+        . (Join-Path $scriptRoot 'TestResultAnalyzer.ps1')
+        $md = GetBcptSummaryMD -path $bcptFilename
+        $md | should -Match 'No baseline provided'
+        [regex]::Matches($md, '\|SUITE1\|').Count | should -Be 1
+        [regex]::Matches($md, '\|Codeunit.\|').Count | should -Be 2
+        [regex]::Matches($md, '\|Operation.\|').Count | should -Be 6
+        [regex]::Matches($md, '\|').Count | should -Be (7*8)
+        
+#         -Contain 'No baseline provided'
+#        $md | should -Contain '|SUITE1|'
+
+#        $md.Replace('\n',"`n") | Set-Content -Path "C:\temp\bcpt.md" -Encoding UTF8
+
+    }
+
 }
