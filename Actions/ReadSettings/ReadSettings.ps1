e1b3ca0f32f4e0f7903b270e1a3c48bcd77801b5
freddydk <freddydk@users.noreply.github.com>
2023-08-07 14:26:06 +0200

    settingValue

--- Actions/ReadSettings/ReadSettings.ps1
+++ Actions/ReadSettings/ReadSettings.ps1
@@ -82,7 +82,7 @@ try {
             if ($settingValue -is [System.Collections.Specialized.OrderedDictionary]) {
                 Add-Content -Encoding UTF8 -Path $env:GITHUB_ENV -Value "$setting=$($settingValue | ConvertTo-Json -Depth 99 -Compress)"
             }
-            elseif ($settingsValue -and ($settingsValue.contains("`n") -or $settingsValue.contains("`r"))) {
+            elseif ($settingValue.contains("`n") -or $settingValue.contains("`r")) {
                 throw "Setting $setting contains line breaks, which is not supported"
             }
             else {
