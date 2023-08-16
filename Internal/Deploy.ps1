f3d69d507d017100a262efb5bd856a7b667a2b19
Alexander Holstrup <117829001+aholstrup1@users.noreply.github.com>
2023-08-16 13:29:46 +0200

    Updates to AL-Go deployment process  (#659)
    This PR changes the deployment process to
    * Create pull requests when deploying Al-Go rather than force pushing 
    * Fetch the OrgPat from a GitHub environment to ensure that the secret
    can only be accessed from workflows running on the main branch of AL-Go
    * (Update the OrgPat to be from a service account rather than a PAT from
    a Microsoft engineer)
    
    New deployment process:
    1. Run the Deploy workflow in microsoft/Al-Go
    2. Request approval from another team member 
    3. The deploy action will by default create PRs in Al-Go-PTE /
    Al-Go-AppSource / Al-Go-Actions. Go to those repositories, validate that
    the PR looks as expected and merge the new changes.
    
    ---------
    
    Co-authored-by: Maria Zhelezova <43066499+mazhelez@users.noreply.github.com>

--- Internal/Deploy.ps1
+++ Internal/Deploy.ps1
@@ -1,13 +1,49 @@
-﻿Param(
+﻿[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSReviewUnusedParameter', 'directCommit', Justification = 'False positive.')]
+Param(
     [Parameter(Mandatory=$true)]
     [Hashtable] $config,
     [Parameter(Mandatory=$true)]
-    [string] $token
+    [string] $token,
+    [Parameter(Mandatory=$false)]
+    [bool] $directCommit
 )
 
 Import-Module (Join-Path $PSScriptRoot "..\Actions\Github-Helper.psm1" -Resolve) -DisableNameChecking
 
 $errorActionPreference = "Stop"; $ProgressPreference = "SilentlyContinue"; Set-StrictMode -Version 2.0
+
+function PushChanges
+(
+    [Parameter(HelpMessage = "The branch Al-Go is being deployed to (e.g. main / v.3.2)", Mandatory = $false)]
+    [string] $BaseBranch,
+    [Parameter(HelpMessage = "The message on the commit or PR that contains the latest changes", Mandatory = $false)]
+    [string] $CommitMessage,
+    [Parameter(HelpMessage = "If true, the commit will be pushed directly to the base branch. If false, a pull request will be created", Mandatory = $false)]
+    [bool] $DirectCommit
+)
+{
+    invoke-git add .
+
+    if ($DirectCommit) {
+        # Direct commit to base branch
+        invoke-git commit --allow-empty -m $CommitMessage
+        invoke-git push origin $BaseBranch
+    } else {
+        # Create PR to base branch
+        if (-not (git ls-remote --heads origin $BaseBranch)) {
+            Write-Host "Branch $BaseBranch does not exist in origin. Creating it"
+            invoke-git branch $BaseBranch origin/main
+            invoke-git push origin $BaseBranch
+        }
+        $branchName = "deploy/$BaseBranch/$((Get-Date).ToUniversalTime().ToString(`"yyMMddHHmmss`"))"
+
+
+        invoke-git checkout -b $branchName origin/$BaseBranch
+        invoke-git commit --allow-empty -m $CommitMessage
+        invoke-git push origin $branchName
+        invoke-gh pr create --base $BaseBranch --title $CommitMessage --body $CommitMessage
+    }
+}
 
 $oldPath = Get-Location
 try {
@@ -198,10 +234,8 @@ try {
         if (Test-Path -Path (Join-Path '.' '.github') -PathType Container) {
             Copy-Item -Path (Join-Path $baseRepoPath "RELEASENOTES.md") -Destination (Join-Path "./.github" "RELEASENOTES.copy.md") -Force
         }
+        PushChanges -BaseBranch $branch -CommitMessage "Deploying AL-Go from $algoBranch ($srcSHA) to $branch" -DirectCommit $directCommit
 
-        invoke-git add .
-        invoke-git commit --allow-empty -m 'checkout'
-        invoke-git push $serverUrl
     }
 }
 finally {
