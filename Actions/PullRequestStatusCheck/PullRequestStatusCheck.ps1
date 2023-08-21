f70e7484b76f0e499d9e27550b4850f3d9760068
aholstrup1 <aholstrup@microsoft.com>
2023-08-21 10:14:27 +0200

    Update env var name and add api version

--- Actions/PullRequestStatusCheck/PullRequestStatusCheck.ps1
+++ Actions/PullRequestStatusCheck/PullRequestStatusCheck.ps1
@@ -7,7 +7,7 @@ param(
 
 Write-Host "Checking workflow status for run $RunId in repository $Repository"
 
-$workflowJobs = gh api /repos/$Repository/actions/runs/$RunId/jobs | ConvertFrom-Json
+$workflowJobs = gh api /repos/$Repository/actions/runs/$RunId/jobs -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" | ConvertFrom-Json
 $failedJobs = $workflowJobs.jobs | Where-Object { $_.conclusion -eq "failure" }
 
 if ($failedJobs) {
