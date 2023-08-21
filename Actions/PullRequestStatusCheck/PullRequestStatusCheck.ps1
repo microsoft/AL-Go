6a65c855cbfba16cad3ff870b0724034e7505f99
Alexander Holstrup <117829001+aholstrup1@users.noreply.github.com>
2023-08-21 07:19:16 +0200

    Update PullRequestStatusCheck.ps1

--- Actions/PullRequestStatusCheck/PullRequestStatusCheck.ps1
+++ Actions/PullRequestStatusCheck/PullRequestStatusCheck.ps1
@@ -7,7 +7,7 @@ param(
 
 Write-Host "gh api /repos/$Repository/actions/runs/$RunId/jobs"
 $workflowJobs = gh api /repos/$Repository/actions/runs/$RunId/jobs | ConvertFrom-Json
-$failedJobs = $workflowJobs | Where-Object { $_.conclusion -eq "failure" }
+$failedJobs = $workflowJobs.jobs | Where-Object { $_.conclusion -eq "failure" }
 
 if ($failedJobs) {
     throw "Workflow failed with the following jobs: $($failedJobs.name -join ', ')"
