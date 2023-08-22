f220fe4c1ffcd2e256cd0ac8ecd0ca7dd2aa1c94
aholstrup1 <aholstrup@microsoft.com>
2023-08-22 11:37:58 +0200

    Update logging

--- Actions/PullRequestStatusCheck/PullRequestStatusCheck.ps1
+++ Actions/PullRequestStatusCheck/PullRequestStatusCheck.ps1
@@ -5,13 +5,13 @@ param(
     [string] $RunId
 )
 
-Write-Host "Checking workflow status for run $RunId in repository $Repository"
+Write-Host "Checking PR Build status for run $RunId in repository $Repository"
 
 $workflowJobs = gh api /repos/$Repository/actions/runs/$RunId/jobs -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" | ConvertFrom-Json
 $failedJobs = $workflowJobs.jobs | Where-Object { $_.conclusion -eq "failure" }
 
 if ($failedJobs) {
-    throw "Workflow failed with the following jobs: $($failedJobs.name -join ', ')"
+    throw "PR Build failed. Failing jobs: $($failedJobs.name -join ', ')"
 }
 
-Write-Host "Workflow succeeded"
+Write-Host "PR Build succeeded"
