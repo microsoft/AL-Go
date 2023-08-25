4f61cce0030cbe5204b777db40a8ed819f609aad
Alexander Holstrup <aholstrup@microsoft.com>
2023-08-25 10:56:11 +0200

    Refactor

--- Actions/ReadSettings/ReadSettings.psm1
+++ Actions/ReadSettings/ReadSettings.psm1
@@ -1,9 +1,5 @@
 function ValidateSettings($Settings) {
-    $ObsoletedSettings = [ordered]@{
-        "*-Projects" = @{ type = "Error"; message = "The setting should be replaced by using the Projects property in the DeployTo* setting in .github/AL-Go-Settings.json instead" }
-        "*_Projects" = @{ type = "Error"; message = "The setting should be replaced by using the Projects property in the DeployTo* setting in .github/AL-Go-Settings.json instead" }
-        "cache*" = @{ type = "Warning"; message =  "The setting 'enableCodeCop' is no longer supported" }
-    }
+    $ObsoletedSettings = Get-Content -Path (Join-Path $PSScriptRoot "ObsoletedSettings.json") | ConvertFrom-Json | ConvertTo-HashTable
 
     $foundError = $false
 
@@ -12,21 +8,17 @@ function ValidateSettings($Settings) {
         $obsoletedSetting = $ObsoletedSettings[$obsoletedSettingName]
 
         # Check if the setting is present in the settings file
-        if ($Settings.Keys -contains $obsoletedSetting) {
-            if ($obsoletedSetting.Type -eq "Warning") {
-                Write-Host "::Warning::The setting $obsoletedSetting has been obsoleted. $($obsoletedSetting.message). This warning will become an error in a future release."
-            } else {
-                Write-Host "::Error::. $($obsoletedSetting.message)."
+        if ($Settings.Keys -contains $obsoletedSettingName) {
+            PrintObsoleteMessage -SettingName $obsoletedSettingName -Message $obsoletedSetting.message -Type $obsoletedSetting.Type
+            if ($obsoletedSetting.Type -eq "Error") {
                 $foundError = $true
             }
         }
 
         # Check if there's a setting that matches the pattern
         $Settings.Keys | Where-Object { $_ -like $obsoletedSettingName } | ForEach-Object {
-            if ($obsoletedSetting.Type -eq "Warning") {
-                Write-Host "::Warning::The setting $_ has been obsoleted. $($obsoletedSetting.message). This warning will become an error in a future release."
-            } else {
-                Write-Host "::Error::$($obsoletedSetting.message)."
+            PrintObsoleteMessage -Setting $_ -Message $obsoletedSetting.message -Type $obsoletedSetting.Type
+            if ($obsoletedSetting.Type -eq "Error") {
                 $foundError = $true
             }
         }
@@ -34,6 +26,14 @@ function ValidateSettings($Settings) {
 
     # Throw an error if any obsolete settings were found
     if ($foundError) {
-        throw "Obsolete settings found. Please remove them from your settings file."
+        throw "Obsolete settings found. Please update your Al-Go settings."
+    }
+}
+
+function PrintObsoleteMessage($SettingName, $Message, $Type) {
+    if ($Type -eq "Warning") {
+        Write-Host "::Warning::The setting $SettingName has been obsoleted. $Message This warning will become an error in a future release."
+    } else {
+        Write-Host "::Error::The setting $SettingName has been obsoleted. $Message"
     }
 }
