c802b782bb219866ed67e3dfc07674a955ae11c3
Alexander Holstrup <aholstrup@microsoft.com>
2023-08-25 11:57:54 +0200

    Add secret validation

--- Actions/ReadSecrets/ReadSecretsHelper.psm1
+++ Actions/ReadSecrets/ReadSecretsHelper.psm1
@@ -220,3 +220,43 @@ function GetSecret {
     Write-Host  "Could not find secret $secret in Github secrets or Azure Key Vault."
     return $null
 }
+
+function ValidateSecrets() {
+    $Secrets = $script:gitHubSecrets.PSObject.Properties.Name
+    $ObsoletedSecrets = Get-Content -Path (Join-Path $PSScriptRoot "ObsoletedSecrets.json") | ConvertFrom-Json | ConvertTo-HashTable
+    $foundError = $false
+
+    $ObsoletedSecrets.Keys | ForEach-Object {
+        $obsoletedSecretName = $_
+        $obsoletedSetting = $ObsoletedSecrets[$obsoletedSecretName]
+
+        # Check if the setting is present in the settings file
+        if ($Secrets -contains $obsoletedSecretName) {
+            PrintObsoleteMessage -SettingName $obsoletedSecretName -Message $obsoletedSetting.message -Type $obsoletedSetting.Type
+            if ($obsoletedSetting.Type -eq "Error") {
+                $foundError = $true
+            }
+        }
+
+        # Check if there's a setting that matches the pattern
+        $Secrets | Where-Object { $_ -like $obsoletedSecretName } | ForEach-Object {
+            PrintObsoleteMessage -Setting $_ -Message $obsoletedSetting.message -Type $obsoletedSetting.Type
+            if ($obsoletedSetting.Type -eq "Error") {
+                $foundError = $true
+            }
+        }
+    }
+
+    # Throw an error if any obsolete settings were found
+    if ($foundError) {
+        throw "Obsolete settings found. Please update your Al-Go settings."
+    }
+}
+
+function PrintObsoleteMessage($SettingName, $Message, $Type) {
+    if ($Type -eq "Warning") {
+        Write-Host "::Warning::The Al-Go setting $SettingName has been obsoleted. $Message This warning will become an error in a future release."
+    } else {
+        Write-Host "::Error::The Al-Go setting $SettingName has been obsoleted. $Message"
+    }
+}
