670c9f307c0a472b5bf571eebd0871fa36b1c2d4
freddydk <freddydk@users.noreply.github.com>
2023-09-06 23:01:57 +0200

    err

--- Tests/WorkflowSanitation/WorkflowFileContent.Test.ps1
+++ Tests/WorkflowSanitation/WorkflowFileContent.Test.ps1
@@ -2,6 +2,10 @@
 Import-Module (Join-Path $PSScriptRoot '../TestActionsHelper.psm1')
 
 Describe "All AL-GO workflows should have similar content" {
+    BeforeEach {
+        $errorActionPreference = "Stop"; $ProgressPreference = "SilentlyContinue"; Set-StrictMode -Version 2.0
+    }
+
     It 'All workflows existing in both templates should have similar content' {
         $pteWorkflows = (Join-Path $PSScriptRoot "..\..\Templates\Per Tenant Extension\.github\workflows\" -Resolve) | GetWorkflowsInPath
         $appSourceWorkflows = ((Join-Path $PSScriptRoot "..\..\Templates\AppSource App\.github\workflows\" -Resolve) | GetWorkflowsInPath).Name
