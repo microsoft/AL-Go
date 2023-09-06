1cae5999f758385fa43db242f01d36853bc6bc06
freddydk <freddydk@users.noreply.github.com>
2023-09-06 23:06:08 +0200

    use pester

--- Tests/WorkflowSanitation/WorkflowFileContent.Test.ps1
+++ Tests/WorkflowSanitation/WorkflowFileContent.Test.ps1
@@ -2,10 +2,6 @@
 Import-Module (Join-Path $PSScriptRoot '../TestActionsHelper.psm1')
 
 Describe "All AL-GO workflows should have similar content" {
-    BeforeEach {
-        $errorActionPreference = "Stop"; $ProgressPreference = "SilentlyContinue"; Set-StrictMode -Version 2.0
-    }
-
     It 'All workflows existing in both templates should have similar content' {
         $pteWorkflows = (Join-Path $PSScriptRoot "..\..\Templates\Per Tenant Extension\.github\workflows\" -Resolve) | GetWorkflowsInPath
         $appSourceWorkflows = ((Join-Path $PSScriptRoot "..\..\Templates\AppSource App\.github\workflows\" -Resolve) | GetWorkflowsInPath).Name
