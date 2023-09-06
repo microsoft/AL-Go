963cdf688c167352c261a26c42ca10af24bb6bab
freddydk <freddydk@users.noreply.github.com>
2023-09-06 23:07:56 +0200

    get-content

--- Tests/WorkflowSanitation/WorkflowFileContent.Test.ps1
+++ Tests/WorkflowSanitation/WorkflowFileContent.Test.ps1
@@ -6,8 +6,8 @@ Describe "All AL-GO workflows should have similar content" {
         $pteWorkflows = (Join-Path $PSScriptRoot "..\..\Templates\Per Tenant Extension\.github\workflows\" -Resolve) | GetWorkflowsInPath
         $appSourceWorkflows = ((Join-Path $PSScriptRoot "..\..\Templates\AppSource App\.github\workflows\" -Resolve) | GetWorkflowsInPath).Name
         $pteWorkflows | Where-Object { $appSourceWorkflows -contains $_.Name } | ForEach-Object {
-            $pteWorkflowContent = Get-ContentLF -Path $_.FullName
-            $appSourceWorkflowContent = Get-ContentLF -Path (Join-Path $PSScriptRoot "..\..\Templates\AppSource App\.github\workflows\$($_.Name)")
+            $pteWorkflowContent = (Get-Content -Path $_.FullName -Encoding UTF8 -Raw).Replace("`r", "").TrimEnd("`n")
+            $appSourceWorkflowContent = (Get-Content -Path (Join-Path $PSScriptRoot "..\..\Templates\AppSource App\.github\workflows\$($_.Name)") -Encoding UTF8 -Raw).Replace("`r", "").TrimEnd("`n")
             Write-Host "Comparing $($_.Name)"
             $pteWorkflowContent | Should -Be $appSourceWorkflowContent
         }
