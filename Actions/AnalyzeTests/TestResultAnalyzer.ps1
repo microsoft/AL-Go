f8fc0dd327e025efb80c24dedc1fb3ee1ec50e34
freddydk <freddydk@users.noreply.github.com>
2023-09-07 11:38:58 +0200

    fix PSSA

--- Actions/AnalyzeTests/TestResultAnalyzer.ps1
+++ Actions/AnalyzeTests/TestResultAnalyzer.ps1
@@ -25,26 +25,25 @@ function GetTestResultSummaryMD {
             if (-not $appNames) {
                 $appNames = @($testResults.testsuites.testsuite | ForEach-Object { $_.Properties.property | Where-Object { $_.Name -eq "extensionId" } | ForEach-Object { $_.Value } } | Select-Object -Unique)
             }
-            $testResults.testsuites.testsuite | ForEach-Object {
-                $totalTests += $_.Tests
-                $totalTime += [decimal]::Parse($_.time, [System.Globalization.CultureInfo]::InvariantCulture)
-                $totalFailed += $_.failures
-                $totalSkipped += $_.skipped
+            foreach($testsuite in $testResults.testsuites.testsuite) {
+                $totalTests += $testsuite.Tests
+                $totalTime += [decimal]::Parse($testsuite.time, [System.Globalization.CultureInfo]::InvariantCulture)
+                $totalFailed += $testsuite.failures
+                $totalSkipped += $testsuite.skipped
             }
             Write-Host "$($appNames.Count) TestApps, $totalTests tests, $totalFailed failed, $totalSkipped skipped, $totalTime seconds"
             $summarySb.Append('|Test app|Tests|Passed|Failed|Skipped|Time|\n|:---|---:|---:|---:|---:|---:|\n') | Out-Null
-            $appNames | ForEach-Object {
-                $appName = $_
+            foreach($appName in $appNames) {
                 $appTests = 0
                 $appTime = 0.0
                 $appFailed = 0
                 $appSkipped = 0
                 $suites = $testResults.testsuites.testsuite | where-Object { $_.Properties.property | Where-Object { $_.Value -eq $appName } }
-                $suites | ForEach-Object {
-                    $appTests += [int]$_.tests
-                    $appFailed += [int]$_.failures
-                    $appSkipped += [int]$_.skipped
-                    $appTime += [decimal]::Parse($_.time, [System.Globalization.CultureInfo]::InvariantCulture)
+                foreach($suite in $suites) {
+                    $appTests += [int]$suite.tests
+                    $appFailed += [int]$suite.failures
+                    $appSkipped += [int]$suite.skipped
+                    $appTime += [decimal]::Parse($suite.time, [System.Globalization.CultureInfo]::InvariantCulture)
                 }
                 $appPassed = $appTests-$appFailed-$appSkipped
                 Write-Host "- $appName, $appTests tests, $appPassed passed, $appFailed failed, $appSkipped skipped, $appTime seconds"
@@ -63,21 +62,21 @@ function GetTestResultSummaryMD {
                 $summarySb.Append("|$($appTime)s|\n") | Out-Null
                 if ($appFailed -gt 0) {
                     $failuresSb.Append("<details><summary><i>$appName, $appTests tests, $appPassed passed, $appFailed failed, $appSkipped skipped, $appTime seconds</i></summary>\n") | Out-Null
-                    $suites | ForEach-Object {
-                        Write-Host "  - $($_.name), $($_.tests) tests, $($_.failures) failed, $($_.skipped) skipped, $($_.time) seconds"
-                        if ($_.failures -gt 0 -and $failuresSb.Length -lt 32000) {
-                            $failuresSb.Append("<details><summary><i>$($_.name), $($_.tests) tests, $($_.failures) failed, $($_.skipped) skipped, $($_.time) seconds</i></summary>") | Out-Null
-                            $_.testcase | ForEach-Object {
-                                if ($_.ChildNodes.Count -gt 0) {
-                                    Write-Host "    - $($_.name), Failure, $($_.time) seconds"
-                                    $failuresSb.Append("<details><summary><i>$($_.name), Failure</i></summary>") | Out-Null
-                                    $_.ChildNodes | ForEach-Object {
-                                        Write-Host "      - Error: $($_.message)"
+                    foreach($suite in $suites) {
+                        Write-Host "  - $($suite.name), $($suite.tests) tests, $($suite.failures) failed, $($suite.skipped) skipped, $($suite.time) seconds"
+                        if ($suite.failures -gt 0 -and $failuresSb.Length -lt 32000) {
+                            $failuresSb.Append("<details><summary><i>$($suite.name), $($suite.tests) tests, $($suite.failures) failed, $($suite.skipped) skipped, $($suite.time) seconds</i></summary>") | Out-Null
+                            foreach($testcase in $suite.testcase) {
+                                if ($testcase.ChildNodes.Count -gt 0) {
+                                    Write-Host "    - $($testcase.name), Failure, $($testcase.time) seconds"
+                                    $failuresSb.Append("<details><summary><i>$($testcase.name), Failure</i></summary>") | Out-Null
+                                    foreach($failure in $testcase.ChildNodes) {
+                                        Write-Host "      - Error: $($failure.message)"
                                         Write-Host "        Stacktrace:"
-                                        Write-Host "        $($_."#text".Trim().Replace("`n","`n        "))"
-                                        $failuresSb.Append("<i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Error: $($_.message)</i><br/>") | Out-Null
+                                        Write-Host "        $($failure."#text".Trim().Replace("`n","`n        "))"
+                                        $failuresSb.Append("<i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Error: $($failure.message)</i><br/>") | Out-Null
                                         $failuresSb.Append("<i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Stack trace</i><br/>") | Out-Null
-                                        $failuresSb.Append("<i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$($_."#text".Trim().Replace("`n","<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"))</i><br/>") | Out-Null
+                                        $failuresSb.Append("<i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$($failure."#text".Trim().Replace("`n","<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"))</i><br/>") | Out-Null
                                     }
                                     $failuresSb.Append("</details>") | Out-Null
                                 }
@@ -120,11 +119,11 @@ function ReadBcptFile {
     $bcptResult = Get-Content -Path $path -Encoding UTF8 | ConvertFrom-Json
     $suites = @{}
     # Sort by bcptCode, codeunitID, operation
-    $bcptResult | ForEach-Object {
-        $bcptCode = $_.bcptCode
-        $codeunitID = $_.codeunitID
-        $codeunitName = $_.codeunitName
-        $operation = $_.operation
+    foreach($measure in $bcptResult) {
+        $bcptCode = $measure.bcptCode
+        $codeunitID = $measure.codeunitID
+        $codeunitName = $measure.codeunitName
+        $operation = $measure.operation
 
         # Create Suite if it doesn't exist
         if(-not $suites.containsKey($bcptCode)) {
@@ -145,8 +144,8 @@ function ReadBcptFile {
         }
         # Add measurement to measurements under operation
         $suites."$bcptCode"."$codeunitID"."operations"."$operation".measurements += @(@{
-            "durationMin" = $_.durationMin
-            "numberOfSQLStmts" = $_.numberOfSQLStmts
+            "durationMin" = $measure.durationMin
+            "numberOfSQLStmts" = $measure.numberOfSQLStmts
         })
     }
     $suites
@@ -182,15 +181,12 @@ function GetBcptSummaryMD {
     $lastOperationName = ''
 
     # calculate statistics on measurements, skipping the $skipMeasurements longest measurements
-    $bcpt.Keys | ForEach-Object {
-        $suiteName = $_
+    foreach($suiteName in $bcpt.Keys) {
         $suite = $bcpt."$suiteName"
-        $suite.Keys | ForEach-Object {
-            $codeUnitID = $_
+        foreach($codeUnitID in $suite.Keys) {
             $codeunit = $suite."$codeunitID"
             $codeUnitName = $codeunit.codeunitName
-            $codeunit."operations".Keys | ForEach-Object {
-                $operationName = $_
+            foreach($operationName in $codeunit."operations".Keys) {
                 $operation = $codeunit."operations"."$operationName"
                 # Get measurements to use for statistics
                 $measurements = @($operation."measurements" | Sort-Object -Descending { $_.durationMin } | Select-Object -Skip $skipMeasurements)
