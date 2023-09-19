c004935ee39882529bda1e708dd31afbdc2a9513
freddydk <freddydk@users.noreply.github.com>
2023-09-19 23:24:47 +0200

    add to updatefiles

--- Actions/CheckForUpdates/CheckForUpdates.ps1
+++ Actions/CheckForUpdates/CheckForUpdates.ps1
@@ -178,7 +178,9 @@ foreach($checkfile in $checkfiles) {
             if ($srcPath -eq '.AL-Go' -and $type -eq "script" -and $realSrcFolder) {
                 Write-Host "Update Project Settings"
                 # Copy settings from the indirect template repository (if the setting doesn't exist in the project folder)
-                UpdateSettingsFile -settingsFile (Join-Path $dstFolder "settings.json") -updateSettings @{} -otherSettings $indirectTemplateProjectSettings
+                $projectSettingsFile = Join-Path $dstFolder "settings.json"
+                UpdateSettingsFile -settingsFile $projectSettingsFile -updateSettings @{} -otherSettings $indirectTemplateProjectSettings
+                $updateFiles += @{ "DstFile" = Join-Path $dstPath "settings.json"; "content" = (Get-Content -Path $projectSettingsFile -Encoding UTF8 -Raw) }
             }
             # Loop through all files in the template repository matching the pattern
             Get-ChildItem -Path $srcFolder -Filter $checkfile.pattern | ForEach-Object {
