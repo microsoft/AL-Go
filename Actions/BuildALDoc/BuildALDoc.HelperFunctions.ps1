249f79acf40ce324c0967d1e80eacf74f8a8cb8b
freddydk <freddydk@users.noreply.github.com>
2023-10-04 12:31:44 +0200

    add build

---
+++ Actions/BuildALDoc/BuildALDoc.HelperFunctions.ps1
@@ -0,0 +1,132 @@
+﻿function DownloadAlDoc {
+    $artifactUrl = Get-BCArtifactUrl -storageAccount bcinsider -type sandbox -country core -select Latest -accept_insiderEula
+    $folder = Download-Artifacts $artifactUrl
+    $alLanguageVsix = Join-Path $folder '*.vsix' -Resolve
+    $tempFolder = Join-Path ([System.IO.Path]::GetTempPath()) ([Guid]::NewGuid().ToString())
+    New-Item -Path $tempFolder -ItemType Directory | Out-Null
+    Expand-Archive -Path $alLanguageVsix -DestinationPath $tempFolder -Force
+    $isPsCore = $PSVersionTable.PSVersion -ge "6.0.0"
+    if ($isPsCore -and $isLinux) {
+        $script:aldocPath = Join-Path $tempFolder 'extension/bin/Linux/aldoc'
+    }
+    else {
+        $script:aldocPath = Join-Path $tempFolder 'extension/bin/win32/aldoc.exe'
+    }
+}
+
+function SanitizeFileName([string] $filename) {
+    $filename.Replace('_','-').Replace('?','_').Replace('*','_').Replace(' ','-').Replace('\','-').Replace('/','-').Replace(':','-').Replace('<','-').Replace('>','-').Replace('|','-').Replace('%','pct')
+}
+
+function GetAppNameAndFolder {
+    Param(
+        [string] $appFile
+    )
+
+    $tmpFolder = Join-Path ([System.IO.Path]::GetTempPath()) ([Guid]::NewGuid().ToString())
+    Extract-AppFileToFolder -appFilename $appFile -appFolder $tmpFolder -generateAppJson
+    $appJson = Get-Content -Path (Join-Path $tmpFolder 'app.json') -Encoding utf8 | ConvertFrom-Json
+    $appJson.name
+    (SanitizeFileName -filename $appJson.name).ToLower()
+    Remove-Item -Path $tmpFolder -Recurse -Force
+}
+
+function GenerateDocsSite {
+    Param(
+        [string] $version,
+        [string[]] $allVersions,
+        [hashtable] $allApps,
+        [string] $releaseNotes,
+        [string] $header = "Documentation",
+        [string] $footer = "Made with <a href=""https://aka.ms/AL-Go"">AL-Go for GitHub</a>, <a href=""https://go.microsoft.com/fwlink/?linkid=2247728"">ALDoc</a> and <a href=""https://dotnet.github.io/docfx"">DocFx</a>",
+        [string] $docsPath,
+        [string] $logLevel,
+        [switch] $hostIt
+    )
+
+    $docfxPath = Join-Path ([System.IO.Path]::GetTempPath()) ([Guid]::NewGuid().ToString())
+    New-Item -path $docfxPath -ItemType Directory | Out-Null
+    try {
+        $apps = @()
+        # Generate new toc.yml and calculate apps - releases and projects
+        $newTocYml = @('items:','  - name: Releases','    items:','    - name: main','      href: /')
+        foreach($ver in $allVersions) {
+            $newTocYml += @("    - name: $ver","      href: /releases/$ver")
+        }
+        if ($allApps.Keys.Count -eq 1 -and $allApps.Keys[0] -eq '.') {
+            # Single prokect repo
+            foreach($appFile in $allApps.".") {
+                $apps += @($appFile)
+                $appName, $appFolder = GetAppNameAndFolder -appFile $appFile
+                $newTocYml += @("  - name: $appName","    href: reference/$appFolder/toc.yml")
+            }
+        }
+        else {
+            # Multi project repo add all apps
+            foreach($project in $allApps.Keys) {
+                $newTocYml += @("  - name: $project",'    items:')
+                foreach($appFile in $allApps."$project") {
+                    $apps += @($appFile)
+                    $appName, $appFolder = GetAppNameAndFolder -appFile $appFile
+                    $newTocYml += @("    - name: $appName","      href: reference/$appFolder/toc.yml")
+                }
+            }
+        }
+
+        CmdDo -command $script:aldocPath -arguments @("init","--output ""$docfxpath""","--loglevel $loglevel","--targetpackages ""$($apps -join '","')""")
+
+        # Update docfx.json
+        $docfxJsonFile = Join-Path $docfxPath 'docfx.json'
+        $docfxJson = Get-Content -Encoding utf8 -Path $docfxJsonFile | ConvertFrom-Json
+        $docfxJson.build.globalMetadata._appName = $header
+        $docfxJson.build.globalMetadata._appFooter = $footer
+        $docfxJson | ConvertTo-Json -Depth 99 | Set-Content -Path $docfxJsonFile -Encoding utf8
+
+        # Create new toc.yml
+        $tocYmlFile = Join-Path $docfxpath 'toc.yml'
+        Set-Content -Path $tocYmlFile -Value ($newTocYml -join "`n") -Encoding utf8
+
+        $apps | ForEach-Object {
+            CmdDo -command $script:aldocPath -arguments @("build","--output ""$docfxpath""","--loglevel $loglevel","--source ""$_""")
+        }
+
+        # Set release notes
+        Set-Content -path (Join-Path $docfxpath 'index.md') -value $releaseNotes -encoding utf8
+
+        $arguments = @("build", "--output ""$docsPath""", "--logLevel $loglevel", $docfxJsonFile)
+        if ($hostIt) {
+            $arguments += @('-s')
+            Write-Host "Generate and host site"
+        }
+        CmdDo -command docfx -arguments $arguments
+    }
+    finally {
+        Remove-Item -Path $docfxPath -Recurse -Force
+    }
+}
+
+function CalculateProjectsAndApps {
+    Param(
+        [string] $tempFolder,
+        [string] $projects,
+        [string] $refname
+    )
+
+    if ($projects -eq "") { $projects = "*" }
+    $projectList = @($projects.Split(',') | ForEach-Object { $_.Replace('\','_').Replace('/','_') })
+    foreach($mask in 'Apps','Dependencies') {
+        $allApps = @{}
+        Get-ChildItem -Path $tempFolder -Directory | ForEach-Object {
+            if ($_.Name -match "^(.*)-main-$mask-(\d*\.\d*\.\d*\.\d*)$") {
+                $project = $Matches[1]
+                if ($projectList -contains $project) {
+                    $allApps.Add($project, @())
+                    Get-ChildItem -Path $_.FullName -Filter '*.app' -Recurse | ForEach-Object {
+                        $allApps[$project] += @($_.FullName)
+                    }
+                }
+            }
+        }
+        $allApps
+    }
+}
--- Actions/BuildALDoc/BuildALDoc.ps1
+++ Actions/BuildALDoc/BuildALDoc.ps1
@@ -4,64 +4,133 @@
 )
 
 . (Join-Path -Path $PSScriptRoot -ChildPath "..\AL-Go-Helper.ps1" -Resolve)
+. (Join-Path -Path $PSScriptRoot -ChildPath "BuildALDoc.HelperFunctions.ps1" -Resolve)
 DownloadAndImportBcContainerHelper
+
+$alDocPath = DownloadAlDoc
 
 $projects = '*'
 $maxReleases = 2
 $artifactsFolder = Join-Path $ENV:GITHUB_WORKSPACE ".artifacts"
 
-# locate all artifacts folders in .artifacts
-Write-Host "CURRENT:"
-Get-ChildItem -Path $artifactsFolder -Recurse -File | ForEach-Object { Write-Host "- $($_.FullName)" }
-$apps = @()
-$dependencies = @()
-if (Test-Path $artifactsFolder -PathType Container) {
-    $projects.Split(',') | ForEach-Object {
-        $project = $_.Replace('\','_').Replace('/','_')
-        $refname = "$ENV:GITHUB_REF_NAME".Replace('/','_')
-        Write-Host "project '$project'"
-        $apps += @((Get-ChildItem -Path $artifactsFolder -Filter "$project-$refname-Apps-*.*.*.*") | ForEach-Object { $_.FullName })
-        if (!($apps)) {
-            throw "There are no build artifacts present in .artifacts matching $project-$refname-Apps-<version>."
-        }
-        $dependencies += @((Get-ChildItem -Path $artifactsFolder -Filter "$project-$refname-Dependencies-*.*.*.*") | ForEach-Object { $_.FullName })
-    }
-}
-else {
-    throw "No build artifacts present in .artifacts."
+$releases = @()
+if ($maxReleases -gt 0) {
+    $releases = GetReleases -token $token -api_url $ENV:GITHUB_API_URL -repository $ENV:GITHUB_REPOSITORY | Where-Object { -not ($_.prerelease -or $_.draft) } | Select-Object -First $maxReleases
 }
 
-# locate all apps in the artifacts folders
-$allApps = @{}
-foreach($folder in $apps) {
-    $projectName = [System.IO.Path]::GetFileName($folder).Split("-$refname-Apps-")[0]
-    $allApps."$projectName" = @(Get-ChildItem -Path (Join-Path $folder '*.app') | Select-Object -ExpandProperty FullName)
-}
+#$docsPath = Join-Path $ENV:GITHUB_WORKSPACE ".aldoc"
+#New-Item $docsPath -ItemType Directory | Out-Null
 
-$releases = GetReleases -token $token -api_url $ENV:GITHUB_API_URL -repository $ENV:GITHUB_REPOSITORY |
-    Where-Object { -not ($_.prerelease -or $_.draft) } |
-    Select-Object -First $maxReleases
+$docsPath = $ENV:GITHUB_WORKSPACE
+$loglevel = 'Verbose'
 
 foreach($release in $releases) {
     $tempFolder = Join-Path ([System.IO.Path]::GetTempPath()) ([Guid]::NewGuid().ToString())
     New-Item -Path $tempFolder -ItemType Directory | Out-Null
-    foreach($mask in 'Apps', 'Dependencies') {
-        DownloadRelease -token $token -projects $projects -api_url $ENV:GITHUB_API_URL -repository $ENV:GITHUB_REPOSITORY -release $release -path $tempFolder -mask $mask -unpack
+    try {
+        foreach($mask in 'Apps', 'Dependencies') {
+            DownloadRelease -token $token -projects $projects -api_url $ENV:GITHUB_API_URL -repository $ENV:GITHUB_REPOSITORY -release $release -path $tempFolder -mask $mask -unpack
+        }
+        Write-Host "$($release.Name):"
+        Get-ChildItem -Path $tempFolder -Recurse -File | ForEach-Object { Write-Host "- $($_.FullName.Substring($tempFolder.Length))" }
+        $allApps,$allDependencies = CalculateProjectsAndApps -tempFolder $tempFolder -projects $projects -refname $ENV:GITHUB_REF_NAME
+        Write-Host "-------------------------"
+        $allApps | Out-Host
+        Write-Host "-------------------------"
+        $allDependencies | Out-Host
+        Write-Host "-------------------------"
     }
-    Write-Host $release.Name
-    Get-ChildItem -Path $tempFolder -Recurse -File | ForEach-Object { Write-Host "- $($_.FullName)" }
+    finally {
+        Remove-Item -Path $tempFolder -Recurse -Force
+    }
+    $version = $release.Name
+    $header = "Documentation for $ENV:GITHUB_REPOSITORY $version"
+    $releaseNotes = $release.body
+    GenerateDocsSite -version $version -allVersions $versions -allApps $allApps -releaseNotes $releaseNotes -header $header -docsPath $docsPath -logLevel $logLevel
+    do {
+        try {
+            $retry = $false
+            Start-Sleep -Seconds 2
+            Rename-Item -Path (join-Path $docsPath "_site") -NewName $version
+        }
+        catch {
+            $retry = $true    
+        }
+    } while ($retry)
 }
 
+$releasesPath = Join-Path $docsPath "_site/releases"
+New-Item -Path $releasesPath -ItemType Directory | Out-Null
+foreach($version in $versions) {
+    Move-Item  -Path (join-Path $docsPath $version) -Destination $releasesPath
+}
+
+Write-Host "main:"
+Get-ChildItem -Path $artifactsFolder -Recurse -File | ForEach-Object { Write-Host "- $($_.FullName.Substring($artifactsFolder.Length))" }
+$allApps,$allDependencies = CalculateProjectsAndApps -tempFolder $artifactsFolder -projects $projects -refname $ENV:GITHUB_REF_NAME
+
+Write-Host "-------------------------"
 $allApps | Out-Host
+Write-Host "-------------------------"
+$allDependencies | Out-Host
+Write-Host "-------------------------"
 
-Write-Host "Apps to build documentation for"
-$apps | Out-Host
+$version = 'main'
+$header = "Documentation for $ENV:GITHUB_REPOSITORY"
+$releaseNotes = "Documentation for current branch"
+GenerateDocsSite -version $version -allVersions $versions -allApps $allApps -releaseNotes $releaseNotes -header $header -docsPath $docsPath -logLevel $logLevel
 
-Write-Host "Dependencies"
-$dependencies | Out-Host
+## locate all artifacts folders in .artifacts
+#Write-Host "CURRENT:"
+#Get-ChildItem -Path $artifactsFolder -Recurse -File | ForEach-Object { Write-Host "- $($_.FullName)" }
+#$apps = @()
+#$dependencies = @()
+#if (Test-Path $artifactsFolder -PathType Container) {
+#    $projects.Split(',') | ForEach-Object {
+#        $project = $_.Replace('\','_').Replace('/','_')
+#        $refname = "$ENV:GITHUB_REF_NAME".Replace('/','_')
+#        Write-Host "project '$project'"
+#        $apps += @((Get-ChildItem -Path $artifactsFolder -Filter "$project-$refname-Apps-*.*.*.*") | ForEach-Object { $_.FullName })
+#        if (!($apps)) {
+#            throw "There are no build artifacts present in .artifacts matching $project-$refname-Apps-<version>."
+#        }
+#        $dependencies += @((Get-ChildItem -Path $artifactsFolder -Filter "$project-$refname-Dependencies-*.*.*.*") | ForEach-Object { $_.FullName })
+#    }
+#}
+#else {
+#    throw "No build artifacts present in .artifacts."
+#}
 
-Write-Host "Releases"
-$releases | Out-Host
-
+# locate all apps in the artifacts folders
+#$allApps = @{}
+#foreach($folder in $apps) {
+#    $projectName = [System.IO.Path]::GetFileName($folder).Split("-$refname-Apps-")[0]
+#    $allApps."$projectName" = @(Get-ChildItem -Path (Join-Path $folder '*.app') | Select-Object -ExpandProperty FullName)
+#}
+#
+#$releases = GetReleases -token $token -api_url $ENV:GITHUB_API_URL -repository $ENV:GITHUB_REPOSITORY |
+#    Where-Object { -not ($_.prerelease -or $_.draft) } |
+#    Select-Object -First $maxReleases
+#
+#foreach($release in $releases) {
+#    $tempFolder = Join-Path ([System.IO.Path]::GetTempPath()) ([Guid]::NewGuid().ToString())
+#    New-Item -Path $tempFolder -ItemType Directory | Out-Null
+#    foreach($mask in 'Apps', 'Dependencies') {
+#        DownloadRelease -token $token -projects $projects -api_url $ENV:GITHUB_API_URL -repository $ENV:GITHUB_REPOSITORY -release $release -path $tempFolder -mask $mask -unpack
+#    }
+#    Write-Host $release.Name
+#    Get-ChildItem -Path $tempFolder -Recurse -File | ForEach-Object { Write-Host "- $($_.FullName)" }
+#}
+#
+#$allApps | Out-Host
+#
+#Write-Host "Apps to build documentation for"
+#$apps | Out-Host
+#
+#Write-Host "Dependencies"
+#$dependencies | Out-Host
+#
+#Write-Host "Releases"
+#$releases | Out-Host
 
 throw "Not implemented"
