f2986d1c24f719d651f2fc55fb271c355fce563a
freddydk <freddydk@users.noreply.github.com>
2023-10-08 12:25:50 +0200

    remove exception

--- Actions/BuildALDoc/BuildALDoc.ps1
+++ Actions/BuildALDoc/BuildALDoc.ps1
@@ -78,5 +78,3 @@ else {
     $releaseNotes = ''
 }
 GenerateDocsSite -version '' -allVersions $versions -allApps $allApps -repoName $settings.repoName -releaseNotes $releaseNotes -header $header -docsPath $docsPath -logLevel $logLevel
-
-throw 'x'
