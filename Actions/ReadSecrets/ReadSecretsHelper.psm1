ce32ebd59a8f74c0087ad67ffed08d08f6899924
freddydk <freddydk@users.noreply.github.com>
2023-12-05 10:11:49 +0100

    S

--- Actions/ReadSecrets/ReadSecretsHelper.psm1
+++ Actions/ReadSecrets/ReadSecretsHelper.psm1
@@ -8,7 +8,7 @@ $script:keyvaultConnectionExists = $false
 $script:azureRm210 = $false
 $script:isKeyvaultSet = $script:gitHubSecrets.PSObject.Properties.Name -eq "AZURE_CREDENTIALS"
 $script:escchars = @(' ','!','\"','#','$','%','\u0026','\u0027','(',')','*','+',',','-','.','/','0','1','2','3','4','5','6','7','8','9',':',';','\u003c','=','\u003e','?','@','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','[','\\',']','^','_',[char]96,'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','{','|','}','~')
-$script:notSecretProperties = @("Scopes","TenantId","BlobName","ContainerName","StorageAccountName","serverUrl")
+$script:notSecretProperties = @("Scopes","TenantId","BlobName","ContainerName","StorageAccountName","ServerUrl")
 
 function IsPropertySecret {
     param (
