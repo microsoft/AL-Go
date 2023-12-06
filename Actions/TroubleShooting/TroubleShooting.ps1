5a32985110b56262cd5385d3baec4cd5fa504a40
freddydk <freddydk@users.noreply.github.com>
2023-12-06 05:24:40 +0100

    addtosummary

--- Actions/TroubleShooting/TroubleShooting.ps1
+++ Actions/TroubleShooting/TroubleShooting.ps1
@@ -3,17 +3,42 @@
     [string] $gitHubSecrets = ""
 )
 
-$errors = @()
-$warnings = @()
-$suggestions = @()
-
 . (Join-Path -Path $PSScriptRoot -ChildPath "..\AL-Go-Helper.ps1" -Resolve)
+
+$global:errors = @()
+$global:warnings = @()
+$global:suggestions = @()
+
+function AddToSummary {
+    Param (
+        [Parameter(Mandatory = $true)]
+        [string] $Message,
+        [Parameter(Mandatory = $true)]
+        [ValidateSet('Error', 'Warning', 'Suggestion')]
+        [string] $type
+    )
+
+    switch($type) {
+        'Error' {
+            $global:errors += "- $Message"
+            Write-Host "Error: $Message"
+        }
+        'Warning' {
+            $global:warnings += "- $Message"
+            Write-Host "Warning: $Message"
+        }
+        'Suggestion' {
+            $global:suggestions += "- $Message"
+            Write-Host "Suggestion: $Message"
+        }
+    }
+}
 
 . (Join-Path -Path $PSScriptRoot -ChildPath "TroubleShoot.Secrets.ps1" -Resolve) -gitHubSecrets ($gitHubSecrets | ConvertFrom-Json)
 
-if ($errors.Count -eq 0) { $errors = @("No errors found") }
-if ($warnings.Count -eq 0) { $warnings = @("No warnings found") }
-if ($suggestions.Count -eq 0) { $suggestions = @("No suggestions found") }
+if ($global:errors.Count -eq 0) { $global:errors = @("No errors found") }
+if ($global:warnings.Count -eq 0) { $global:warnings = @("No warnings found") }
+if ($global:suggestions.Count -eq 0) { $global:suggestions = @("No suggestions found") }
 
-$summaryMD = (@("# Errors") + $errors + @("# Warnings") + $warnings + @("# Suggestions") + $suggestions) -join "`n`n"
+$summaryMD = (@("# Errors") + $global:errors + @("# Warnings") + $global:warnings + @("# Suggestions") + $global:suggestions) -join "`n`n"
 Set-Content $ENV:GITHUB_STEP_SUMMARY -Value $summaryMD
