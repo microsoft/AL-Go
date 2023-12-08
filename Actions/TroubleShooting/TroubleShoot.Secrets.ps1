9fe50d5f5e7774b15a1279f6481401ee07914b74
freddydk <freddydk@users.noreply.github.com>
2023-12-08 08:51:18 +0100

    use string

--- Actions/TroubleShooting/TroubleShoot.Secrets.ps1
+++ Actions/TroubleShooting/TroubleShoot.Secrets.ps1
@@ -53,7 +53,7 @@ function CheckSecretForCommonMistakes {
         # JSON Secrets properties should not contain values shorter then $minSecretSize characters
         foreach($keyName in $json.PSObject.Properties.Name) {
             if (IsPropertySecret -propertyName $keyName) {
-                if ($json."$keyName".Length -lt $minSecretSize) {
+                if ("$($json."$keyName")".Length -lt $minSecretSize) {
                     OutputWarning -Message "JSON Secret $displayName contains properties with very short values. These values will be masked, but the secret might be indirectly exposed and might also cause issues in AL-Go for GitHub."
                     $warning = $true
                 }
