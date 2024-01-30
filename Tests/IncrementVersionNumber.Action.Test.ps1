bac2fa44856b1d69b78ec638fb406a90637b01e4
mazhelez <mazhelez@microsoft.com>
2024-01-30 15:48:53 +0100

    Add tests for Set-SettingInFile

--- Tests/IncrementVersionNumber.Action.Test.ps1
+++ Tests/IncrementVersionNumber.Action.Test.ps1
@@ -27,5 +27,76 @@ Describe "IncrementVersionNumber Action Tests" {
     }
 
     # Call action
+}
 
+Describe "Set-SettingInFile tests" {
+    BeforeAll {
+        . (Join-Path -Path $PSScriptRoot -ChildPath "..\Actions\AL-Go-Helper.ps1" -Resolve)
+        Import-Module (Join-Path -path $PSScriptRoot -ChildPath "..\Actions\IncrementVersionNumber\IncrementVersionNumber.psm1" -Resolve) -Force
+    }
+
+    BeforeEach {
+        # Create test JSON settings file in the temp folder
+        $settingsFilePath = Join-Path ([System.IO.Path]::GetTempPath()) "$([Guid]::NewGuid().ToString()).json"
+        $settingsFileContent = [ordered]@{
+            "repoVersion" = "0.1"
+            "otherSetting" = "otherSettingValue"
+        }
+        $settingsFileContent | ConvertTo-Json | Set-Content $settingsFilePath -Encoding UTF8
+    }
+
+    It 'Set-SettingInFile -settingsFilePath not found' {
+        $nonExistingFile = Join-Path ([System.IO.Path]::GetTempPath()) "UnknownFile.json"
+        $settingName = 'repoVersion'
+        $newValue = '1.0'
+        $incremental = $false
+
+        { Set-SettingInFile -settingsFilePath $nonExistingFile -settingName $settingName -newValue $newValue -incremental:$incremental } | Should -Throw "Settings file ($nonExistingFile) not found."
+    }
+
+    It 'Set-SettingInFile -settingName not found' {
+        $settingName = 'repoVersion2'
+        $newValue = '1.0'
+        $incremental = $false
+
+        { Set-SettingInFile -settingsFilePath $settingsFilePath -settingName $settingName -newValue $newValue -incremental:$incremental } | Should -Not -Throw
+
+        $newSettingsContent = Get-Content $settingsFilePath -Encoding UTF8 | ConvertFrom-Json
+        $newSettingsContent | Should -Not -Contain $settingName
+        # Check that the other setting are not changed
+        $newSettingsContent.otherSetting | Should -Be "otherSettingValue"
+        $newSettingsContent.repoVersion | Should -Be "0.1"
+    }
+
+    It 'Set-SettingInFile -newValue is set' {
+        $settingName = 'repoVersion'
+        $newValue = '1.0'
+        $incremental = $false
+
+        Set-SettingInFile -settingsFilePath $settingsFilePath -settingName $settingName -newValue $newValue -incremental:$incremental
+
+        $newSettingsContent = Get-Content $settingsFilePath -Encoding UTF8 | ConvertFrom-Json
+        $newSettingsContent.$settingName | Should -Be "1.0"
+
+        # Check that the other setting are not changed
+        $newSettingsContent.otherSetting | Should -Be "otherSettingValue"
+    }
+
+    It 'Set-SettingInFile -newValue is added to the old value' {
+        $settingName = 'repoVersion'
+        $newValue = '0.1'
+        $incremental = $true
+
+        Set-SettingInFile -settingsFilePath $settingsFilePath -settingName $settingName -newValue $newValue -incremental:$incremental
+
+        $newSettingsContent = Get-Content $settingsFilePath -Encoding UTF8 | ConvertFrom-Json
+        $newSettingsContent.$settingName | Should -Be "0.2"
+
+        # Check that the other setting are not changed
+        $newSettingsContent.otherSetting | Should -Be "otherSettingValue"
+    }
+
+    AfterEach {
+        Remove-Item $settingsFilePath -Force
+    }
 }
