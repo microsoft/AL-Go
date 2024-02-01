7a0ac9592ca7bbd9f6d1ca2d34c0639e5417da85
mazhelez <mazhelez@microsoft.com>
2024-02-01 16:04:38 +0100

    Add/improve tests

--- Tests/IncrementVersionNumber.Action.Test.ps1
+++ Tests/IncrementVersionNumber.Action.Test.ps1
@@ -33,16 +33,24 @@ Describe "Set-VersionSettingInFile tests" {
     BeforeAll {
         . (Join-Path -Path $PSScriptRoot -ChildPath "..\Actions\AL-Go-Helper.ps1" -Resolve)
         Import-Module (Join-Path -path $PSScriptRoot -ChildPath "..\Actions\IncrementVersionNumber\IncrementVersionNumber.psm1" -Resolve) -Force
-    }
 
-    BeforeEach {
-        # Create test JSON settings file in the temp folder
-        $settingsFilePath = Join-Path ([System.IO.Path]::GetTempPath()) "$([Guid]::NewGuid().ToString()).json"
-        $settingsFileContent = [ordered]@{
-            "repoVersion" = "0.1"
-            "otherSetting" = "otherSettingValue"
+        $settingsFilePath = "<not set>" # Might be used in tests, used in AfterEach
+
+        function New-TestSettingsFile {
+            param(
+                [string] $repoVersion = '0.1'
+            )
+
+            # Create test JSON settings file in the temp folder
+            $settingsFilePath = Join-Path ([System.IO.Path]::GetTempPath()) "$([Guid]::NewGuid().ToString()).json"
+            $settingsFileContent = [ordered]@{
+                "repoVersion" = $repoVersion
+                "otherSetting" = "otherSettingValue"
+            }
+            $settingsFileContent | ConvertTo-Json | Set-Content $settingsFilePath -Encoding UTF8
+
+            return $settingsFilePath
         }
-        $settingsFileContent | ConvertTo-Json | Set-Content $settingsFilePath -Encoding UTF8
     }
 
     It 'Set-VersionSettingInFile -settingsFilePath not found' {
@@ -54,6 +62,7 @@ Describe "Set-VersionSettingInFile tests" {
     }
 
     It 'Set-VersionSettingInFile -settingName not found' {
+        $settingsFilePath = New-TestSettingsFile
         $settingName = 'repoVersion2'
         $newValue = '1.0'
 
@@ -67,6 +76,7 @@ Describe "Set-VersionSettingInFile tests" {
     }
 
     It 'Set-VersionSettingInFile -newValue is set' {
+        $settingsFilePath = New-TestSettingsFile
         $settingName = 'repoVersion'
         $newValue = '1.0'
 
@@ -80,6 +90,7 @@ Describe "Set-VersionSettingInFile tests" {
     }
 
     It 'Set-VersionSettingInFile -newValue is added to the old value (minor version)' {
+        $settingsFilePath = New-TestSettingsFile
         $settingName = 'repoVersion'
         $newValue = '+0.2'
 
@@ -93,6 +104,7 @@ Describe "Set-VersionSettingInFile tests" {
     }
 
     It 'Set-VersionSettingInFile -newValue is added to the old value (major value)' {
+        $settingsFilePath = New-TestSettingsFile
         $settingName = 'repoVersion'
         $newValue = '+1.0'
 
@@ -105,7 +117,49 @@ Describe "Set-VersionSettingInFile tests" {
         $newSettingsContent.otherSetting | Should -Be "otherSettingValue"
     }
 
+    It 'Set-VersionSettingInFile -newValue is added to the old value (major and minor value)' {
+        $settingsFilePath = New-TestSettingsFile -repoVersion '1.2'
+        $settingName = 'repoVersion'
+        $newValue = '+3.4'
+
+        Set-VersionSettingInFile -settingsFilePath $settingsFilePath -settingName $settingName -newValue $newValue
+
+        $newSettingsContent = Get-Content $settingsFilePath -Encoding UTF8 | ConvertFrom-Json
+        $newSettingsContent.$settingName | Should -Be "4.6"
+
+        # Check that the other setting are not changed
+        $newSettingsContent.otherSetting | Should -Be "otherSettingValue"
+    }
+
+    It 'Set-VersionSettingInFile -newValue is set and build and revision are kept from the old value'{
+        $settingsFilePath = New-TestSettingsFile -repoVersion '1.2.0.0'
+        $settingName = 'repoVersion'
+        $newValue = '2.1'
+
+        Set-VersionSettingInFile -settingsFilePath $settingsFilePath -settingName $settingName -newValue $newValue
+
+        $newSettingsContent = Get-Content $settingsFilePath -Encoding UTF8 | ConvertFrom-Json
+        $newSettingsContent.$settingName | Should -Be "2.1.0.0"
+
+        # Check that the other setting are not changed
+        $newSettingsContent.otherSetting | Should -Be "otherSettingValue"
+    }
+
+    It 'Set-VersionSettingInFile -newValue is added and build and revision are kept from the old value' {
+        $settingsFilePath = New-TestSettingsFile -repoVersion '1.2.3.4'
+        $settingName = 'repoVersion'
+        $newValue = '+0.1'
+
+        Set-VersionSettingInFile -settingsFilePath $settingsFilePath -settingName $settingName -newValue $newValue
+
+        $newSettingsContent = Get-Content $settingsFilePath -Encoding UTF8 | ConvertFrom-Json
+        $newSettingsContent.$settingName | Should -Be "1.3.3.4"
+
+        # Check that the other setting are not changed
+        $newSettingsContent.otherSetting | Should -Be "otherSettingValue"
+    }
+
     AfterEach {
-        Remove-Item $settingsFilePath -Force
+        Remove-Item $settingsFilePath -Force -ErrorAction Ignore
     }
 }
