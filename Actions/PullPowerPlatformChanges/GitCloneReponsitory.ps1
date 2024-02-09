1fe1841de5c3d61136c02898a9903b77ea7aef16
freddydk <freddydk@users.noreply.github.com>
2024-02-09 18:39:46 +0100

    pull

--- Actions/PullPowerPlatformChanges/GitCloneReponsitory.ps1
+++ Actions/PullPowerPlatformChanges/GitCloneReponsitory.ps1
@@ -1,46 +1,26 @@
 Param(
     [Parameter(HelpMessage = "The GitHub actor running the action", Mandatory = $false)]
-    [string] $Actor,
+    [string] $actor,
     [Parameter(HelpMessage = "The GitHub token running the action", Mandatory = $false)]
-    [string] $Token,
-    [Parameter(HelpMessage = "Specifies the parent telemetry scope for the telemetry signal", Mandatory = $false)]
-    [string] $ParentTelemetryScopeJson = '7b7d',
-    [Parameter(HelpMessage = "Direct Commit (Y/N)", Mandatory = $false)]
-    [string] $DirectCommit
+    [string] $token,
+    [Parameter(HelpMessage = "Set the branch to update", Mandatory = $false)]
+    [string] $updateBranch,
+    [Parameter(HelpMessage = "Direct Commit", Mandatory = $false)]
+    [bool] $directCommit
 )
 
-$ErrorActionPreference = "Stop"
-Set-StrictMode -Version 2.0
-$telemetryScope = $ParentTelemetryScopeJson;
-Write-Host "Starting GitCloneRepository.ps1 with parameters: $([environment]::Newline)Actor: $Actor$([environment]::Newline)Token: $Token$([environment]::Newline)ParentTelemetryScopeJson: $ParentTelemetryScopeJson$([environment]::Newline)DirectCommit: $DirectCommit"
+$ErrorActionPreference = "Stop"; $ProgressPreference = "SilentlyContinue"; Set-StrictMode -Version 2.0
+
+Write-Host "Starting GitCloneRepository.ps1 with parameters: $([environment]::Newline)actor: $actor$([environment]::Newline)token: $token$([environment]::Newline)updateBranch: $updateBranch$([environment]::Newline)directCommit: $directCommit"
 
 # Import the helper script
 . (Join-Path -Path $PSScriptRoot -ChildPath "..\AL-Go-Helper.ps1" -Resolve)
 
+Write-Host "Cloning the repository into a new folder"
+$serverUrl, $branch = CloneIntoNewFolder -actor $actor -token $token -updateBranch $updateBranch -DirectCommit $directCommit -newBranchPrefix "PowerPlatform-$($type.replace(' ','-').ToLowerInvariant())"
+$baseFolder = (Get-Location).Path
 
-# IMPORTANT: No code that can fail should be outside the try/catch
-try {    
-    $gitHubBranch = ""    
-    if ($DirectCommit -eq "false") {
-        $gitHubBranch = [System.IO.Path]::GetRandomFileName()
-        Write-Host "Creating a new branch: $gitHubBranch"
-    }
-    
-    Write-Host "Cloning the repository into a new folder"
-    $serverUrl = CloneIntoNewFolder -Actor $Actor -Token $Token -Branch $gitHubBranch
-    $baseFolder = (Get-Location).Path
-
-    Add-Content -Path $env:GITHUB_ENV -Value "clonedRepoPath=$baseFolder"
-    Add-Content -Path $env:GITHUB_ENV -Value "serverUrl=$serverUrl"
-    Add-Content -Path $env:GITHUB_ENV -Value "gitHubBranch=$gitHubBranch"
-    #TrackTrace -telemetryScope $telemetryScope
-}
-catch {
-    Write-Error -message "Pull changes failed.$([environment]::Newline)Error: $($_.Exception.Message)$([environment]::Newline)Stacktrace: $($_.scriptStackTrace)"
-    # TODO: Why can we not find the trackExceptions function?
-    #TrackException -telemetryScope $telemetryScope -errorRecord $_
-}
-finally {
-
-}
+Add-Content -encoding utf8 -Path $env:GITHUB_ENV -Value "clonedRepoPath=$baseFolder"
+Add-Content -encoding utf8 -Path $env:GITHUB_ENV -Value "serverUrl=$serverUrl"
+Add-Content -encoding utf8 -Path $env:GITHUB_ENV -Value "gitHubBranch=$branch"
         
--- Actions/PullPowerPlatformChanges/GitCommitChanges.ps1
+++ Actions/PullPowerPlatformChanges/GitCommitChanges.ps1
@@ -1,50 +1,33 @@
 Param(
     [Parameter(HelpMessage = "The GitHub actor running the action", Mandatory = $false)]
-    [string] $Actor,
+    [string] $actor,
     [Parameter(HelpMessage = "The GitHub token running the action", Mandatory = $false)]
-    [string] $Token,
-    [Parameter(HelpMessage = "Specifies the parent telemetry scope for the telemetry signal", Mandatory = $false)]
-    [string] $ParentTelemetryScopeJson = '7b7d',
+    [string] $token,
     [Parameter(HelpMessage = "The name of the environment as defined in GitHub", Mandatory = $false)]
-    [string] $PowerPlatformSolutionName,
+    [string] $powerPlatformSolutionName,
     [Parameter(HelpMessage = "The name of the environment as defined in GitHub", Mandatory = $false)]
-    [string] $EnvironmentName,
+    [string] $environmentName,
     [Parameter(HelpMessage = "The current location for files to be checked in", Mandatory = $false)]
-    [string] $Location,
-    [Parameter(HelpMessage = "The current location for files to be checked in", Mandatory = $false)]
-    [string] $ServerUrl,
-    [Parameter(HelpMessage = "The current location for files to be checked in", Mandatory = $false)]
-    [string] $GitHubBranch
+    [string] $location,
+    [Parameter(HelpMessage = "ServerUrl for Git Push", Mandatory = $false)]
+    [string] $serverUrl,
+    [Parameter(HelpMessage = "Branch to update", Mandatory = $false)]
+    [string] $gitHubBranch
 )
 
-$ErrorActionPreference = "Stop"
-Set-StrictMode -Version 2.0
-$telemetryScope = $ParentTelemetryScopeJson;
-Write-Host "Starting GitHubCommitChanges.ps1 with parameters: $([environment]::Newline)Actor: $Actor$([environment]::Newline)Token: $Token$([environment]::Newline)ParentTelemetryScopeJson: $ParentTelemetryScopeJson$([environment]::Newline)EnvironmentName: $EnvironmentName$([environment]::Newline)Location: $Location$([environment]::Newline)ServerUrl: $ServerUrl$([environment]::Newline)GitHubBranch: $GitHubBranch"
+$ErrorActionPreference = "Stop"; $ProgressPreference = "SilentlyContinue"; Set-StrictMode -Version 2.0
+
+Write-Host "Starting GitHubCommitChanges.ps1 with parameters: $([environment]::Newline)actor: $Actor$([environment]::Newline)Token: $Token$([environment]::Newline)$([environment]::Newline)EnvironmentName: $EnvironmentName$([environment]::Newline)Location: $Location$([environment]::Newline)ServerUrl: $ServerUrl$([environment]::Newline)GitHubBranch: $GitHubBranch"
 
 # Import the helper script
 . (Join-Path -Path $PSScriptRoot -ChildPath "..\AL-Go-Helper.ps1" -Resolve)
 
+Set-Location -Path $location
 
-# IMPORTANT: No code that can fail should be outside the try/catch
-try {
-    Set-Location -Path $Location
+# Environment variables for hub commands
+$env:GITHUB_USER = $actor
+$env:GITHUB_TOKEN = $token
 
-    # Environment variables for hub commands
-    $env:GITHUB_USER = $Actor
-    $env:GITHUB_TOKEN = $Token
-
-    # Commit from the new folder
-    write-host "Committing changes from the new folder $Location\$PowerPlatformSolutionName to branch $GitHubBranch"
-    CommitFromNewFolder -ServerUrl $serverUrl -CommitMessage "Update solution: $PowerPlatformSolutionName with latest from environment: $EnvironmentName" -Branch $GitHubBranch
-    # TODO: Why can we not find the trackTrace function?
-    #TrackTrace -telemetryScope $telemetryScope
-}
-catch {
-    Write-Error -message "Pull changes failed.$([environment]::Newline)Error: $($_.Exception.Message)$([environment]::Newline)Stacktrace: $($_.scriptStackTrace)"
-    # TODO: Why can we not find the trackExceptions function?
-    #TrackException -telemetryScope $telemetryScope -errorRecord $_
-}
-finally {
-
-}        
+# Commit from the new folder
+write-host "Committing changes from the new folder $Location\$PowerPlatformSolutionName to branch $GitHubBranch"
+CommitFromNewFolder -ServerUrl $serverUrl -CommitMessage "Update solution: $PowerPlatformSolutionName with latest from environment: $EnvironmentName" -Branch $GitHubBranch
