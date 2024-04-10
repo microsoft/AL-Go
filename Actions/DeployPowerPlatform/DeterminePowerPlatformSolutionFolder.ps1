f1ebc3a5895858c3eb7cd06e6a245259cf9eb35e
freddydk <freddydk@users.noreply.github.com>
2024-04-10 21:21:32 +0200

    PSA

--- Actions/DeployPowerPlatform/DeterminePowerPlatformSolutionFolder.ps1
+++ Actions/DeployPowerPlatform/DeterminePowerPlatformSolutionFolder.ps1
@@ -9,11 +9,11 @@ if ($artifactsFolder -ne '') {
       ## Find file containing Power Platform keyword
       if($filePath.contains("-PowerPlatformSolution-")){
           Write-Host "Power Platform solution file:"$filePath
-          Add-Content -encoding utf8 -path $env:GITHUB_ENV -value "powerPlatformSolutionFilePath=$filePath" 
+          Add-Content -encoding utf8 -path $env:GITHUB_ENV -value "powerPlatformSolutionFilePath=$filePath"
           Add-Content -encoding utf8 -path $env:GITHUB_ENV -value "powerPlatformSolutionFolder=.artifacts/_tempPPSolution/source"
           return
       }
-  }            
+  }
   throw "Not able to find Power Platform solution file in $artifactFolder that contains the artifact keyword '-PowerPlatformSolution-'"
 }
 elseif ($solutionFolder -ne '') {
