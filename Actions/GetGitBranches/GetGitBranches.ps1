14be5915e1fd255c501395d37047efc262dbbf98
mazhelez <mazhelez@microsoft.com>
2024-04-15 22:21:40 +0200

    Add -returnValue param to invoke-git

--- Actions/GetGitBranches/GetGitBranches.ps1
+++ Actions/GetGitBranches/GetGitBranches.ps1
@@ -7,7 +7,7 @@ $gitHubHelperPath = Join-Path $PSScriptRoot '../Github-Helper.psm1' -Resolve
 Import-Module $gitHubHelperPath -DisableNameChecking
 
 invoke-git fetch
-$allBranches = @(invoke-git for-each-ref --format="%(refname:short)" refs/remotes/origin | ForEach-Object { $_ -replace 'origin/', '' })
+$allBranches = @(invoke-git -returnValue for-each-ref --format="%(refname:short)" refs/remotes/origin | ForEach-Object { $_ -replace 'origin/', '' })
 
 $includeBranches = ConvertFrom-Json $includeBranchesJson
 if ($includeBranches) {
