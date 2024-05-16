9bc199329be416e523fce9e99b650ea24dfb383f
aholstrup1 <aholstrup@microsoft.com>
2024-05-16 09:43:04 +0200

    Uptake in PP

--- Actions/Invoke-AlGoAction.ps1
+++ Actions/Invoke-AlGoAction.ps1
@@ -1,5 +1,6 @@
 param(
-    [scriptblock]$Action
+    [scriptblock]$Action,
+    [switch]$SkipTelemetry
 )
 
 $errorActionPreference = "Stop"
@@ -11,11 +12,15 @@ Import-Module (Join-Path -Path $PSScriptRoot -ChildPath "TelemetryHelper.psm1" -
 try {
     Invoke-Command -ScriptBlock $Action
 
-    # Log telemetry
-    Trace-Information
+    if (-not $SkipTelemetry) {
+        Trace-Information
+    }
 }
 catch {
-    Trace-Exception -ErrorRecord $_
+    if (-not $SkipTelemetry) {
+        Trace-Exception -ErrorRecord $_
+    }
+
     Write-Host "::ERROR::Unexpected error when running action. Error Message: $($_.Exception.Message.Replace("`r",'').Replace("`n",' ')), StackTrace: $($_.ScriptStackTrace.Replace("`r",'').Replace("`n",' <- '))";
     exit 1
 }
