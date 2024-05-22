a82e778a69797635d6d125e8d43c821d708f7b16
freddydk <freddydk@users.noreply.github.com>
2024-05-22 19:20:52 +0200

    updatebranch

--- Actions/CheckForUpdates/CheckForUpdates.ps1
+++ Actions/CheckForUpdates/CheckForUpdates.ps1
@@ -206,7 +206,7 @@ else {
     try {
         if (!$directCommit) {
             $env:GH_TOKEN = $token
-            $existingPullRequest = gh api --paginate /repos/$env:GITHUB_REPOSITORY/pulls -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" | ConvertFrom-Json | Where-Object { $_.title -eq "Update AL-Go System Files - $templateSha" -and $_.base.ref -eq $branch }
+            $existingPullRequest = gh api --paginate /repos/$env:GITHUB_REPOSITORY/pulls -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" | ConvertFrom-Json | Where-Object { $_.title -eq "Update AL-Go System Files - $templateSha" -and $_.base.ref -eq $updateBranch }
             if ($existingPullRequest) {
                 OutputWarning "Pull request already exists for updated AL-Go System Files."
                 exit
