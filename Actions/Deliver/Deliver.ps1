ca0e59141c41f4cb802711465837d700c54ae7b6
freddydk <freddydk@users.noreply.github.com>
2024-05-31 14:12:18 +0200

    dump

--- Actions/Deliver/Deliver.ps1
+++ Actions/Deliver/Deliver.ps1
@@ -421,7 +421,7 @@ try {
                     throw "appSourceContext secret is missing"
                 }
                 if (!$appSourceContext.ContainsKey('clientSecret')) {
-                    Write-Host "Actions_id_token_request_url: $ENV:ACTIONS_ID_TOKEN_REQUEST_URL"
+                    Write-Host "Query ID_TOKEN from $ENV:ACTIONS_ID_TOKEN_REQUEST_URL"
                     $result = Invoke-RestMethod -Method GET -UseBasicParsing -Headers @{ "Authorization" = "bearer $ENV:ACTIONS_ID_TOKEN_REQUEST_TOKEN"; "Accept" = "application/vnd.github+json" } -Uri "$ENV:ACTIONS_ID_TOKEN_REQUEST_URL&audience=api://AzureADTokenExchange"
                     $appSourceContext += @{
                         "clientAssertion" = $result.value
