24fde865d448a1572bacf5b94ea98c166ca97174
freddydk <freddydk@users.noreply.github.com>
2024-06-03 06:23:37 +0200

    syntax

--- Actions/ReadSecrets/ReadSecretsHelper.psm1
+++ Actions/ReadSecrets/ReadSecretsHelper.psm1
@@ -177,7 +177,7 @@ function ConnectAzureKeyVault {
         Clear-AzContext -Scope Process
         Clear-AzContext -Scope CurrentUser -Force -ErrorAction SilentlyContinue
         if ($keyVaultCredentials.PSObject.Properties.Name -eq 'ClientAssertion') {
-            Connect-AzAccount -ApplicationId -keyVaultCredentials.ClientId -Tenant $keyVaultCredentials.TenantId -FederatedToken $keyVaultCredentials.ClientAssertion #-WarningAction SilentlyContinue | Out-Null
+            Connect-AzAccount -ApplicationId $keyVaultCredentials.ClientId -Tenant $keyVaultCredentials.TenantId -FederatedToken $keyVaultCredentials.ClientAssertion #-WarningAction SilentlyContinue | Out-Null
             Set-AzContext -SubscriptionId $keyVaultCredentials.SubscriptionId -Tenant $keyVaultCredentials.TenantId #-ErrorAction SilentlyContinue -WarningAction SilentlyContinue | Out-Null
         }
         else {
