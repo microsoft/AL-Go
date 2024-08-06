8ec6169f460f7a674c42bb7123ae5e6add2b9964
Alexander Holstrup <117829001+aholstrup1@users.noreply.github.com>
2024-08-06 13:21:31 +0200

    Update AL-Go Telemetry Module (#1049)
    - Log telemetry without needing bccontainerhelper
    - Remove parentTelemetryScopeJson inputs to AL-Go actions
    - Add a bit more GitHub context to telemetry

--- Actions/TelemetryHelper.psm1
+++ Actions/TelemetryHelper.psm1
@@ -1,87 +1,208 @@
-$signals = @{
-    "DO0070" = "AL-Go action ran: AddExistingApp"
-    "DO0071" = "AL-Go action ran: CheckForUpdates"
-    "DO0072" = "AL-Go action ran: CreateApp"
-    "DO0073" = "AL-Go action ran: CreateDevelopmentEnvironment"
-    "DO0074" = "AL-Go action ran: CreateReleaseNotes"
-    "DO0075" = "AL-Go action ran: Deploy"
-    "DO0076" = "AL-Go action ran: IncrementVersionNumber"
-    "DO0077" = "AL-Go action ran: PipelineCleanup"
-    "DO0078" = "AL-Go action ran: ReadSecrets"
-    "DO0079" = "AL-Go action ran: ReadSettings"
-    "DO0080" = "AL-Go action ran: RunPipeline"
-    "DO0081" = "AL-Go action ran: Deliver"
-    "DO0082" = "AL-Go action ran: AnalyzeTests"
-    "DO0083" = "AL-Go action ran: Sign"
+. (Join-Path -Path $PSScriptRoot -ChildPath ".\AL-Go-Helper.ps1" -Resolve)
+Import-Module (Join-Path $PSScriptRoot '.\Github-Helper.psm1' -Resolve)
 
-    "DO0084" = "AL-Go action ran: DetermineArtifactUrl"
-    "DO0085" = "AL-Go action ran: DetermineProjectsToBuild"
+#region Loading telemetry helper
+function DownloadNugetPackage($PackageName, $PackageVersion) {
+    $nugetPackagePath = Join-Path "$ENV:GITHUB_WORKSPACE" "/.nuget/packages/$PackageName/$PackageVersion/"
 
-    "DO0090" = "AL-Go workflow ran: AddExistingAppOrTestApp"
-    "DO0091" = "AL-Go workflow ran: CICD"
-    "DO0092" = "AL-Go workflow ran: CreateApp"
-    "DO0093" = "AL-Go workflow ran: CreateOnlineDevelopmentEnvironment"
-    "DO0094" = "AL-Go workflow ran: CreateRelease"
-    "DO0095" = "AL-Go workflow ran: CreateTestApp"
-    "DO0096" = "AL-Go workflow ran: IncrementVersionNumber"
-    "DO0097" = "AL-Go workflow ran: PublishToEnvironment"
-    "DO0098" = "AL-Go workflow ran: UpdateGitHubGoSystemFiles"
-    "DO0099" = "AL-Go workflow ran: NextMajor"
-    "DO0100" = "AL-Go workflow ran: NextMinor"
-    "DO0101" = "AL-Go workflow ran: Current"
-    "DO0102" = "AL-Go workflow ran: CreatePerformanceTestApp"
-    "DO0103" = "AL-Go workflow ran: PublishToAppSource"
-    "DO0104" = "AL-Go workflow ran: PullRequestHandler"
-}
+    if (-not (Test-Path -Path $nugetPackagePath)) {
+        $url = "https://www.nuget.org/api/v2/package/$PackageName/$PackageVersion"
 
-Function strToHexStr {
-    Param(
-        [string] $str
-    )
-    $bytes = [System.Text.Encoding]::UTF8.GetBytes($str)
-    $hexStr = [System.Text.StringBuilder]::new($Bytes.Length * 2)
-    ForEach($byte in $Bytes){
-        $hexStr.AppendFormat("{0:x2}", $byte) | Out-Null
-    }
-    $hexStr.ToString()
-}
+        Write-Host "Downloading Nuget package $PackageName $PackageVersion..."
+        New-Item -ItemType Directory -Path $nugetPackagePath | Out-Null
+        Invoke-WebRequest -Uri $Url -OutFile "$nugetPackagePath/$PackageName.$PackageVersion.zip"
 
-Function hexStrToStr {
-    Param(
-        [String] $hexStr
-    )
-    $Bytes = [byte[]]::new($hexStr.Length / 2)
-    For($i=0; $i -lt $hexStr.Length; $i+=2){
-        $Bytes[$i/2] = [convert]::ToByte($hexStr.Substring($i, 2), 16)
-    }
-    [System.Text.Encoding]::UTF8.GetString($Bytes)
-}
+        # Unzip the package
+        Expand-Archive -Path "$nugetPackagePath/$PackageName.$PackageVersion.zip" -DestinationPath "$nugetPackagePath"
 
-function CreateScope {
-    param (
-        [string] $eventId,
-        [string] $parentTelemetryScopeJson = '7b7d'
-    )
-
-    $signalName = $signals[$eventId]
-    if (-not $signalName) {
-        throw "Invalid event id ($eventId) is enountered."
+        # Remove the zip file
+        Remove-Item -Path "$nugetPackagePath/$PackageName.$PackageVersion.zip"
     }
 
-    if ($parentTelemetryScopeJson -and $parentTelemetryScopeJson -ne '7b7d') {
-        RegisterTelemetryScope (hexStrToStr -hexStr $parentTelemetryScopeJson) | Out-Null
-    }
-
-    $telemetryScope = InitTelemetryScope -name $signalName -eventId $eventId  -parameterValues @()  -includeParameters @()
-
-    return $telemetryScope
+    return $nugetPackagePath
 }
 
-function GetHash {
+function LoadApplicationInsightsDll() {
+    $packagePath = DownloadNugetPackage -PackageName "Microsoft.ApplicationInsights" -PackageVersion (GetPackageVersion -PackageName "Microsoft.ApplicationInsights")
+    $AppInsightsDllPath = "$packagePath/lib/net46/Microsoft.ApplicationInsights.dll"
+
+    if (-not (Test-Path -Path $AppInsightsDllPath)) {
+        throw "Failed to download Application Insights DLL"
+    }
+
+    [Reflection.Assembly]::LoadFile($AppInsightsDllPath) | Out-Null
+}
+
+function Get-ApplicationInsightsTelemetryClient($TelemetryConnectionString)
+{
+    # Load the Application Insights DLL
+    LoadApplicationInsightsDll
+
+    $TelemetryClient = [Microsoft.ApplicationInsights.TelemetryClient]::new()
+    $TelemetryClient.TelemetryConfiguration.ConnectionString = $TelemetryConnectionString
+
+    return $TelemetryClient
+}
+#endregion
+
+function AddTelemetryEvent()
+{
     param(
-        [string] $str
+        [Parameter(Mandatory = $true)]
+        [String] $Message,
+        [Parameter(Mandatory = $false)]
+        [System.Collections.Generic.Dictionary[[System.String], [System.String]]] $Data = @{},
+        [Parameter(Mandatory = $false)]
+        [ValidateSet("Information", "Error")]
+        [String] $Severity = 'Information'
     )
 
-    $stream = [IO.MemoryStream]::new([Text.Encoding]::UTF8.GetBytes($str))
-    (Get-FileHash -InputStream $stream -Algorithm SHA256).Hash
+    try {
+        # Add powershell version
+        Add-TelemetryProperty -Hashtable $Data -Key 'PowerShellVersion' -Value ($PSVersionTable.PSVersion.ToString())
+
+        $module = Get-Module BcContainerHelper
+        if ($module) {
+            $versionNoFile = Join-Path -Path (Split-Path $module.Path -Parent) -ChildPath 'Version.txt'
+            Add-TelemetryProperty -Hashtable $Data -Key 'BcContainerHelperVersion' -Value (Get-Content -Path $versionNoFile -Encoding UTF8)
+        }
+
+        Add-TelemetryProperty -Hashtable $Data -Key 'WorkflowName' -Value $ENV:GITHUB_WORKFLOW
+        Add-TelemetryProperty -Hashtable $Data -Key 'RunnerOs' -Value $ENV:RUNNER_OS
+        Add-TelemetryProperty -Hashtable $Data -Key 'RunId' -Value $ENV:GITHUB_RUN_ID
+        Add-TelemetryProperty -Hashtable $Data -Key 'RunNumber' -Value $ENV:GITHUB_RUN_NUMBER
+        Add-TelemetryProperty -Hashtable $Data -Key 'RunAttempt' -Value $ENV:GITHUB_RUN_ATTEMPT
+
+        ### Add GitHub Repository information
+        Add-TelemetryProperty -Hashtable $Data -Key 'Repository' -Value $ENV:GITHUB_REPOSITORY_ID
+
+        $repoSettings = ReadSettings
+        if ($repoSettings.microsoftTelemetryConnectionString -ne '') {
+            Write-Host "Enabling Microsoft telemetry..."
+            $MicrosoftTelemetryClient = Get-ApplicationInsightsTelemetryClient -TelemetryConnectionString $repoSettings.microsoftTelemetryConnectionString
+            $MicrosoftTelemetryClient.TrackTrace($Message, [Microsoft.ApplicationInsights.DataContracts.SeverityLevel]::$Severity, $Data)
+            $MicrosoftTelemetryClient.Flush()
+        }
+
+        if ($repoSettings.partnerTelemetryConnectionString -ne '') {
+            Write-Host "Enabling partner telemetry..."
+            $PartnerTelemetryClient = Get-ApplicationInsightsTelemetryClient -TelemetryConnectionString $repoSettings.partnerTelemetryConnectionString
+            $PartnerTelemetryClient.TrackTrace($Message, [Microsoft.ApplicationInsights.DataContracts.SeverityLevel]::$Severity, $Data)
+            $PartnerTelemetryClient.Flush()
+        }
+    } catch {
+        Write-Host "Failed to log telemetry event: $_"
+    }
 }
+
+<#
+    .SYNOPSIS
+    Logs an information message to telemetry
+
+    .DESCRIPTION
+    Logs an information message to telemetry
+
+    .PARAMETER Message
+    The message to log to telemetry
+
+    .PARAMETER ActionName
+    The name of the action to log to telemetry
+
+    .PARAMETER AdditionalData
+    Additional data to log to telemetry
+
+    .EXAMPLE
+    Trace-Information -Message "AL-Go action ran: $actionName"
+#>
+function Trace-Information() {
+    param(
+        [Parameter(ParameterSetName = 'Message', Mandatory = $true)]
+        [String] $Message,
+        [Parameter(ParameterSetName = 'ActionName', Mandatory = $true)]
+        [String] $ActionName,
+        [Parameter(Mandatory = $false)]
+        [System.Collections.Generic.Dictionary[[System.String], [System.String]]] $AdditionalData = @{}
+    )
+
+    if (-not $Message) {
+        $Message = "AL-Go action ran: $ActionName"
+    }
+
+    AddTelemetryEvent -Message $Message -Severity 'Information' -Data $AdditionalData
+}
+
+<#
+    .SYNOPSIS
+    Logs an exception message to telemetry
+
+    .DESCRIPTION
+    Logs an exception message to telemetry
+
+    .PARAMETER Message
+    The message to log to telemetry
+
+    .PARAMETER ActionName
+    The name of the action to log to telemetry
+
+    .PARAMETER ErrorRecord
+    The error record to log to telemetry
+
+    .PARAMETER AdditionalData
+    Additional data to log to telemetry
+
+    .EXAMPLE
+    Trace-Exception -ErrorRecord $ErrorRecord
+#>
+function Trace-Exception() {
+    param(
+        [Parameter(ParameterSetName = 'Message', Mandatory = $true)]
+        [String] $Message,
+        [Parameter(ParameterSetName = 'ActionName', Mandatory = $true)]
+        [String] $ActionName,
+        [Parameter(Mandatory = $false)]
+        [System.Management.Automation.ErrorRecord] $ErrorRecord = $null,
+        [Parameter(Mandatory = $false)]
+        [System.Collections.Generic.Dictionary[[System.String], [System.String]]] $AdditionalData = @{}
+    )
+
+    if ($ErrorRecord -ne $null) {
+        Add-TelemetryProperty -Hashtable $AdditionalData -Key 'ErrorMessage' -Value $ErrorRecord.Exception.Message
+    }
+
+    if (-not $Message) {
+        $Message = "AL-Go action failed: $ActionName"
+    }
+    AddTelemetryEvent -Message $Message -Severity 'Error' -Data $AdditionalData
+}
+
+<#
+    .SYNOPSIS
+    Adds a key-value pair to a hashtable if the key does not already exist
+
+    .DESCRIPTION
+    Adds a key-value pair to a hashtable if the key does not already exist
+
+    .PARAMETER Hashtable
+    The hashtable to add the key-value pair to
+
+    .PARAMETER Key
+    The key to add to the hashtable
+
+    .PARAMETER Value
+    The value to add to the hashtable
+
+    .EXAMPLE
+    Add-TelemetryProperty -Hashtable $AdditionalData -Key 'RepoType' -Value 'PTE'
+#>
+function Add-TelemetryProperty() {
+    param(
+        [System.Collections.Generic.Dictionary[[System.String], [System.String]]] $Hashtable,
+        [String] $Key,
+        [String] $Value
+    )
+    if (-not $Hashtable.ContainsKey($Key) -and ($Value -ne '')) {
+        $Hashtable.Add($Key, $Value)
+    }
+}
+
+Export-ModuleMember -Function Trace-Information, Trace-Exception, Add-TelemetryProperty
