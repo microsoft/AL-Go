1cf1db3a075934428bf4b69a031db3717e9a6f20
freddydk <freddydk@users.noreply.github.com>
2024-11-10 20:10:11 +0100

    add baseline

--- Actions/DetermineProjectsToBuild/DetermineProjectsToBuild.psm1
+++ Actions/DetermineProjectsToBuild/DetermineProjectsToBuild.psm1
@@ -25,7 +25,7 @@ function Get-ModifiedFiles {
     elseif ($baselineSHA) {
         Write-Host "Not a pull request, using baseline SHA $baselineSHA and current HEAD"
         git fetch origin $baselineSHA | Out-Host
-        $headSHA = git rev-parse HEAD
+        $headSHA = git rev-parse HEADp
         Write-Host "Current HEAD is $headSHA"
     }
     else {
