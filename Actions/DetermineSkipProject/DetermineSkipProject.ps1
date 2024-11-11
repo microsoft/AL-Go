0a7ad8f8bffa3d7f140cedb2fdd079de1851f169
freddydk <freddydk@users.noreply.github.com>
2024-11-11 08:39:19 +0100

    use action

---
+++ Actions/DetermineSkipProject/DetermineSkipProject.ps1
@@ -0,0 +1,32 @@
+﻿Param(
+    [Parameter(HelpMessage = "Compressed JSON string containing the list of projects that should be skipped", Mandatory = $true)]
+    [string] $skippedProjectsJson,
+    [Parameter(HelpMessage = "Name of the project to build", Mandatory = $true)]
+    [string] $project,
+    [Parameter(HelpMessage = "Id of the baseline workflow run, from which to download artifacts if build is skipped", Mandatory = $false)]
+    [string] $baselineWorkflowRunId
+)
+
+. (Join-Path -Path $PSScriptRoot -ChildPath "..\AL-Go-Helper.ps1" -Resolve)
+
+$settings = $env:Settings | ConvertFrom-Json | ConvertTo-HashTable -recurse
+
+$skippedProjects = $skippedProjectsJson | ConvertFrom-Json
+$buildIt = $skippedProjects -notcontains $project
+if (!$buildIt) {
+    # Download the artifacts from the baseline workflow run
+    # Set buildIt to true if the download isn't successful
+    $allArtifacts = @()
+    'Apps','TestApps','Dependencies','PowerPlatformSolution' | ForEach-Object {
+        $allArtifacts += @(GetArtifactsFromWorkflowRun -workflowRun $baselineWorkflowRunId -token $env:GITHUB_TOKEN -api_url $env:GITHUB_API_URL -repository $env:GITHUB_REPOSITORY -mask $_ -projects $project
+    }
+    if ($allArtifacts) {
+        $allArtifacts | Out-Host
+    }
+    else {
+        $buildIt = $true
+    }
+}
+
+Add-Content -Encoding UTF8 -Path $env:GITHUB_OUTPUT -Value "BuildIt=$buildIt"
+Write-Host "BuildIt=$buildIt"
---
+++ Actions/DetermineSkipProject/README.md
@@ -0,0 +1,34 @@
+# Calculate Artifact Names
+
+Calculate Artifact Names for AL-Go workflows
+
+## INPUT
+
+### ENV variables
+
+| Name | Description |
+| :-- | :-- |
+| Settings | env.Settings must be set by a prior call to the ReadSettings Action |
+
+### Parameters
+
+| Name | Required | Description | Default value |
+| :-- | :-: | :-- | :-- |
+| shell | | The shell (powershell or pwsh) in which the PowerShell script in this action should run | powershell |
+| skippedProjectsJson | Yes | Compressed JSON string containing the list of projects that should be skipped | |
+| project | Yes | Name of the project to build | |
+| baselineWorkflowRunId | Yes | Id of the baseline workflow run, from which to download artifacts if build is skipped | |
+
+
+
+## OUTPUT
+
+### ENV variables
+
+none
+
+### OUTPUT variables
+
+| Name | Description |
+| :-- | :-- |
+| BuildIt | True if the project should be built |
