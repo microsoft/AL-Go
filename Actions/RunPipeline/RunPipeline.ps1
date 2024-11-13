3d4e8b214a4ba31c4ce2d0b333c283e95e3edadc
freddydk <freddydk@users.noreply.github.com>
2024-11-14 00:37:18 +0100

    remove dump

--- Actions/RunPipeline/RunPipeline.ps1
+++ Actions/RunPipeline/RunPipeline.ps1
@@ -148,7 +148,6 @@ try {
             Push-Location $ENV:GITHUB_WORKSPACE
             $modifiedFolders = @($settings.appfolders+$settings.testFolders+$settings.bcptTestFolders | Where-Object {
                 $theFolder = Resolve-Path (Join-Path $baseFolder "$project/$_") -Relative
-                Write-Host "Checking $modifiedFiles against '$($theFolder.SubString(2))$([System.IO.Path]::DirectorySeparatorChar)*'"
                 $modifiedFiles -like "$($theFolder.SubString(2))$([System.IO.Path]::DirectorySeparatorChar)*"
             })
             Pop-Location
