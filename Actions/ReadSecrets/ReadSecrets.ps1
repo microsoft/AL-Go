222284acf3c83962fa3bc18e7a1b16a250e80dcf
freddydk <freddydk@users.noreply.github.com>
2024-12-03 07:55:23 +0100

    support non-base64

--- Actions/ReadSecrets/ReadSecrets.ps1
+++ Actions/ReadSecrets/ReadSecrets.ps1
@@ -37,7 +37,7 @@ try {
             # If we are using the ghTokenWorkflow for commits, we need to get ghTokenWorkflow secret
             $secret = 'ghTokenWorkflow'
         }
-        $secretNameProperty = "$($secret.TrimStart('*'))SecretName"
+        $secretNameProperty = "$($secret.TrimStart('-*'))SecretName"
         if ($secret -eq 'AppDependencySecrets') {
             $getAppDependencySecrets = $true
         }
@@ -83,9 +83,11 @@ try {
     foreach($secret in @($secretsCollection)) {
         $secretSplit = $secret.Split('=')
         $secretsProperty = $secretSplit[0]
-        # Secret names preceded by an asterisk are returned encrypted (and base64 encoded)
-        $secretsPropertyName = $secretsProperty.TrimStart('*')
-        $encrypted = $secretsProperty.StartsWith('*')
+        # Secret names preceded by an asterisk are returned encrypted (and base64 encoded unless...)
+        # Secret names preceded by a minus are not base64 encoded
+        $secretsPropertyName = $secretsProperty.TrimStart('-*')
+        $encrypted = $secretsProperty.TrimStart('-').StartsWith('*')
+        $base64encoded = !($secretsProperty.TrimStart('*').StartsWith('-'))
         $secretName = $secretsPropertyName
         if ($secretSplit.Count -gt 1) {
             $secretName = $secretSplit[1]
@@ -120,7 +122,9 @@ try {
                         }
                     }
                 }
-                $base64value = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($secretValue))
+                if ($base64encoded) {
+                    $secretValue = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($secretValue))
+                }
                 $outSecrets += @{ "$secretsProperty" = $base64value }
                 Write-Host "$($secretsPropertyName) successfully read from secret $secretName"
                 $secretsCollection.Remove($secret)
@@ -133,7 +137,7 @@ try {
             $secretSplit = @($_.Split('='))
             $secretsProperty = $secretSplit[0]
             # Secret names preceded by an asterisk are returned encrypted (and base64 encoded)
-            $secretsPropertyName = $secretsProperty.TrimStart('*')
+            $secretsPropertyName = $secretsProperty.TrimStart('-*')
             if ($secretSplit.Count -eq 1 -or ($secretSplit[1] -eq '')) {
                 $secretsPropertyName
             }
