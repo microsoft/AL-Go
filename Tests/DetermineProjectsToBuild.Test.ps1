30575191221401cc401d139872e4633ff51187ec
aholstrup1 <aholstrup@microsoft.com>
2025-01-06 10:40:02 +0100

    Add / Fix tests

--- Tests/DetermineProjectsToBuild.Test.ps1
+++ Tests/DetermineProjectsToBuild.Test.ps1
@@ -606,6 +606,10 @@ Describe "Get-ProjectsToBuild" {
 
         #Add settings file
         $alGoSettings = @{ alwaysBuildAllProjects = $false; projects = @(); powerPlatformSolutionFolder = ''; useProjectDependencies = $true }
+        New-Item -Path "$baseFolder/.github" -type Directory -Force
+        $alGoSettings | ConvertTo-Json -Depth 99 -Compress | Out-File (Join-Path $baseFolder ".github/AL-Go-Settings.json")
+        
+        # Add settings as environment variable to simulate we've run ReadSettings
         $env:Settings = ConvertTo-Json $alGoSettings -Depth 99 -Compress
 
         $allProjects, $projectsToBuild, $projectDependencies, $buildOrder = Get-ProjectsToBuild -baseFolder $baseFolder
@@ -661,6 +665,86 @@ Describe "Get-ProjectsToBuild" {
         $buildOrder[1].buildDimensions[0].project | Should -BeExactly "Project2"
     }
 
+    It 'loads dependent projects correctly, if useProjectDependencies is set to false in a project setting' {
+        # Two dependent projects
+        $dependecyAppFile = @{ id = '83fb8305-4079-415d-a25d-8132f0436fd1'; name = 'First App'; publisher = 'Contoso'; version = '1.0.0.0'; dependencies = @() }
+        New-Item -Path "$baseFolder/Project1/.AL-Go/settings.json" -type File -Force
+        New-Item -Path "$baseFolder/Project1/app/app.json" -Value (ConvertTo-Json $dependecyAppFile -Depth 10) -type File -Force
+
+        $dependantAppFile = @{ id = '83fb8305-4079-415d-a25d-8132f0436fd2'; name = 'Second App'; publisher = 'Contoso'; version = '1.0.0.0'; dependencies = @(@{id = '83fb8305-4079-415d-a25d-8132f0436fd1'; name = 'First App'; publisher = 'Contoso'; version = '1.0.0.0'} ) }
+        New-Item -Path "$baseFolder/Project2/.AL-Go/settings.json" -type File -Force
+        New-Item -Path "$baseFolder/Project2/app/app.json" -Value (ConvertTo-Json $dependantAppFile -Depth 10) -type File -Force
+
+        # Third project that also depends on the first project, but has useProjectDependencies set to false
+        $dependantAppFile = @{ id = '83fb8305-4079-415d-a25d-8132f0436fd3'; name = 'Third App'; publisher = 'Contoso'; version = '1.0.0.0'; dependencies = @(@{id = '83fb8305-4079-415d-a25d-8132f0436fd1'; name = 'First App'; publisher = 'Contoso'; version = '1.0.0.0'} ) }
+        New-Item -Path "$baseFolder/Project3/.AL-Go/settings.json" -type File -Force
+        @{ useProjectDependencies = $false } | ConvertTo-Json -Depth 99 -Compress | Out-File (Join-Path $baseFolder "Project3/.AL-Go/settings.json")
+        New-Item -Path "$baseFolder/Project3/app/app.json" -Value (ConvertTo-Json $dependantAppFile -Depth 10) -type File -Force
+
+        #Add settings file
+        $alGoSettings = @{ alwaysBuildAllProjects = $false; projects = @(); powerPlatformSolutionFolder = ''; useProjectDependencies = $true }
+        New-Item -Path "$baseFolder/.github" -type Directory -Force
+        $alGoSettings | ConvertTo-Json -Depth 99 -Compress | Out-File (Join-Path $baseFolder ".github/AL-Go-Settings.json")
+        
+        # Add settings as environment variable to simulate we've run ReadSettings
+        $env:Settings = ConvertTo-Json $alGoSettings -Depth 99 -Compress
+
+        $allProjects, $projectsToBuild, $projectDependencies, $buildOrder = Get-ProjectsToBuild -baseFolder $baseFolder
+
+        $allProjects | Should -BeExactly @("Project1", "Project2", "Project3")
+        $projectsToBuild | Should -BeExactly @('Project1', 'Project2', 'Project3')
+
+        $projectDependencies | Should -BeOfType System.Collections.Hashtable
+        $projectDependencies['Project1'] | Should -BeExactly @()
+        $projectDependencies['Project2'] | Should -BeExactly @("Project1")
+        $projectDependencies['Project3'] | Should -BeExactly @()
+
+        # Build order should have the following structure:
+        #[
+        #  {
+        #    "projects":  [
+        #      "Project1"
+        #    ],
+        #    "projectsCount":  1,
+        #    "buildDimensions":  [
+        #       {
+        #         "buildMode": "Default",
+        #         "project": "Project1"
+        #       }
+        #    ]
+        #  },
+        #  {
+        #    "projects":  [
+        #      "Project2"
+        #    ],
+        #    "projectsCount":  1,
+        #    "buildDimensions":  [
+        #       {
+        #         "buildMode": "Default",
+        #         "project": "Project2"
+        #       }
+        #    ]
+        #  }
+        #]
+
+        $buildOrder.Count | Should -BeExactly 2
+        $buildOrder[0] | Should -BeOfType System.Collections.Hashtable
+        $buildOrder[0].projects | Should -BeExactly @("Project1", "Project3")
+        $buildOrder[0].projectsCount | Should -BeExactly 2
+        $buildOrder[0].buildDimensions.Count | Should -BeExactly 2
+        $buildOrder[0].buildDimensions[0].buildMode | Should -BeExactly "Default"
+        $buildOrder[0].buildDimensions[0].project | Should -BeExactly "Project1"
+        $buildOrder[0].buildDimensions[1].buildMode | Should -BeExactly "Default"
+        $buildOrder[0].buildDimensions[1].project | Should -BeExactly "Project3"
+
+        $buildOrder[1] | Should -BeOfType System.Collections.Hashtable
+        $buildOrder[1].projects | Should -BeExactly @("Project2")
+        $buildOrder[1].projectsCount | Should -BeExactly 1
+        $buildOrder[1].buildDimensions.Count | Should -BeExactly 1
+        $buildOrder[1].buildDimensions[0].buildMode | Should -BeExactly "Default"
+        $buildOrder[1].buildDimensions[0].project | Should -BeExactly "Project2"
+    }
+
     It 'throws if the calculated build depth is more than the maximum supported' {
         # Two dependent projects
         $dependecyAppFile = @{ id = '83fb8305-4079-415d-a25d-8132f0436fd1'; name = 'First App'; publisher = 'Contoso'; version = '1.0.0.0'; dependencies = @() }
@@ -673,6 +757,10 @@ Describe "Get-ProjectsToBuild" {
 
         #Add settings file
         $alGoSettings = @{ alwaysBuildAllProjects = $false; projects = @(); powerPlatformSolutionFolder = ''; useProjectDependencies = $true }
+        New-Item -Path "$baseFolder/.github" -type Directory -Force
+        $alGoSettings | ConvertTo-Json -Depth 99 -Compress | Out-File (Join-Path $baseFolder ".github/AL-Go-Settings.json")
+        
+        # Add settings as environment variable to simulate we've run ReadSettings
         $env:Settings = ConvertTo-Json $alGoSettings -Depth 99 -Compress
 
         { Get-ProjectsToBuild -baseFolder $baseFolder -maxBuildDepth 1 } | Should -Throw "The build depth is too deep, the maximum build depth is 1. You need to run 'Update AL-Go System Files' to update the workflows"
