04742d1a1a4fee8febd603a8d410f313d0d8d599
freddydk <freddydk@users.noreply.github.com>
2025-01-14 15:25:27 +0100

    check existingbranch

--- e2eTests/scenarios/FederatedCredentials/runtest.ps1
+++ e2eTests/scenarios/FederatedCredentials/runtest.ps1
@@ -64,7 +64,7 @@ $headers = GetHeaders -token $e2epat -repository "$githubOwner/.github"
 $existingBranchJson = gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /repos/$repository/branches/$branch 2> $null
 Write-Host "EXISTING BRANCH JSON: $existingBranchJson"
 $existingBranch = $existingBranchJson | ConvertFrom-Json
-if ($existingBranch.PSObject.Properties.Name -eq 'Name' -and $existingBranch.Name -eq $branch) {
+if ($existingBranch -and $existingBranch.PSObject.Properties.Name -eq 'Name' -and $existingBranch.Name -eq $branch) {
     Write-Host "Removing existing branch $branch"
     Invoke-RestMethod -Method Delete -Uri "https://api.github.com/repos/$repository/git/refs/heads/$branch" -Headers $headers
     Start-Sleep -Seconds 10
