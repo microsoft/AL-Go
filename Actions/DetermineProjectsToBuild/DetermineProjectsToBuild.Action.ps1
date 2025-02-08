4cb540c1fb87243f2167f48aeca8f6eed79e3ce8
freddydk <freddydk@users.noreply.github.com>
2025-02-08 07:12:32 +0100

    give warning if alwysBuild is set

--- Actions/DetermineProjectsToBuild/DetermineProjectsToBuild.Action.ps1
+++ Actions/DetermineProjectsToBuild/DetermineProjectsToBuild.Action.ps1
@@ -22,7 +22,7 @@ $publishSkippedProjects = $true
 if ($ghEventName -eq 'pull_request' -or $ghEventName -eq 'pull_request_target') {
     $branch = $env:GITHUB_BASE_REF
     # DEPRECATION: REMOVE AFTER October 1st 2025 --->
-    if ($settings.PSObject.Properties.Name -eq 'alwaysBuildAllProjects') {
+    if ($settings.PSObject.Properties.Name -eq 'alwaysBuildAllProjects' -and $settings.alwaysBuildAllProjects) {
         $buildAllProjects = $settings.alwaysBuildAllProjects
         Trace-DeprecationWarning -Message "alwaysBuildAllProjects is deprecated" -DeprecationTag "alwaysBuildAllProjects"
     }
