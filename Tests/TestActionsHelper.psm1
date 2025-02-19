770623009ed169e4f1f23fc1658613f0f24e1fb2
Maria Zhelezova <mazhelez@microsoft.com>
2025-02-19 16:46:28 +0100

    Add tests

--- Tests/TestActionsHelper.psm1
+++ Tests/TestActionsHelper.psm1
@@ -307,6 +307,10 @@ function FindMismatchedHashtableValue($Prefix, $ActualValue, $ExpectedValue) {
     }
 }
 
+function GetRepoRoot {
+    return (git rev-parse --show-toplevel)
+}
+
 Add-AssertionOperator -Name MatchHashtable -Test $function:PesterMatchHashtable
 
 Export-ModuleMember -Function GetActionScript
@@ -314,3 +318,4 @@ Export-ModuleMember -Function YamlTest
 Export-ModuleMember -Function GetWorkflowsInPath
 Export-ModuleMember -Function TestActionsReferences
 Export-ModuleMember -Function TestWorkflowReferences
+Export-ModuleMember -Function GetRepoRoot
