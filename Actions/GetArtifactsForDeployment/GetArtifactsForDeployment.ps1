ae6f5d509484ddcfd5fad875718bd072cd3bffdf
Sebastian Petersen <spetersen@microsoft.com>
2025-03-03 18:18:34 +0100

    Using new params in function

--- Actions/GetArtifactsForDeployment/GetArtifactsForDeployment.ps1
+++ Actions/GetArtifactsForDeployment/GetArtifactsForDeployment.ps1
@@ -99,7 +99,7 @@ elseif ($artifactsVersion -like "PR_*") {
     #$downloadArtifacts = $true
     OutputMessageAndArray "Artifacts from pr build:" $prArtifacts
     OutputMessageAndArray "Artifacts from last known good build:" $lastKnownGoodBuildArtifacts
-    DownloadPRArtifacts -token $token -path $artifactsFolder -prArtifacts $prArtifacts -lastKnownGoodBuildArtifacts $lastKnownGoodBuildArtifacts 
+    DownloadPRArtifacts -token $token -path $artifactsFolder -prArtifacts $prArtifacts -lastKnownGoodBuildArtifacts $lastKnownGoodBuildArtifacts -prRunId $latestPRBuildId -lastKnownGoodRunId $lastKnownGoodBuildRunId
 }
 else {
     $searchArtifacts = $true
