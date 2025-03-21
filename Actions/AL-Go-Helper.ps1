66d2a4fcb2e55015c0adb7d1be15d7ec832b5ca8
Maria Zhelezova <mazhelez@microsoft.com>
2025-03-21 13:45:15 +0100

    Include error when PR creation process fails

--- Actions/AL-Go-Helper.ps1
+++ Actions/AL-Go-Helper.ps1
@@ -1432,7 +1432,7 @@ function CommitFromNewFolder {
             }
         }
         catch {
-            OutputError("GitHub actions are not allowed to create Pull Requests (see GitHub Organization or Repository Actions Settings). You can create the PR manually by navigating to $($env:GITHUB_SERVER_URL)/$($env:GITHUB_REPOSITORY)/tree/$branch")
+            OutputError("GitHub actions are not allowed to create Pull Requests (see GitHub Organization or Repository Actions Settings). You can create the PR manually by navigating to $($env:GITHUB_SERVER_URL)/$($env:GITHUB_REPOSITORY)/tree/$branch`nError: $_")
         }
         return $true
     }
