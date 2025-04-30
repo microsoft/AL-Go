aa2e43e624e46872fbff5a196e468609472726c1
jonasblunck <jonasbl@microsoft.com>
2025-04-30 16:43:24 +0200

    Refactor

--- Actions/CheckForNewWarnings/CheckForNewWarnings.ps1
+++ Actions/CheckForNewWarnings/CheckForNewWarnings.ps1
@@ -8,37 +8,70 @@ Param(
     [string] $baselineWorkflowRunId
 )
 
-if (-not $ENV:GITHUB_BASE_REF)
+function GetArtifactMask
 {
-    Write-Host "Checking for warnings only runs on pull requests."
-    return
-}
+    param (
+        [string] $buildOutputFile,
+        [string] $buildMode
+    )
 
-if (-not $settings.failOnNewWarnings)
-{
-    Write-Host "Failing on new warnings is disabled in the settings."
-    return
-}
-
-try
-{
-    Write-Host "::group::Analyzing build for new warnings..."
-
-    Import-Module (Join-Path $PSScriptRoot "..\Github-Helper.psm1" -Resolve) -DisableNameChecking
-    Import-Module (Join-Path $PSScriptRoot ".\CheckForWarningsUtils.psm1" -Resolve) -DisableNameChecking
-
-    Write-Host "Downloading build logs from previous good build."
-
-    if ($project) { $projectName = $project } else { $projectName = $env:GITHUB_REPOSITORY -replace '.+/' }
-    
     $mask = Get-Item $prBuildOutputFile | Select-Object -ExpandProperty BaseName
     if ($buildMode -ne 'Default')
     {
         $mask = "$buildMode$mask"
     }
 
+    return $mask
+}
+
+function GetProjectName
+{
+    param (
+        [string] $project
+    )
+
+    if ($project)
+    {
+        return $project
+    }
+    else
+    {
+        return $env:GITHUB_REPOSITORY -replace '.+/' 
+    }
+}
+
+try
+{
+    Write-Host "::group::Analyzing build for new warnings..."
+
+    if (-not $ENV:GITHUB_BASE_REF)
+    {
+        Write-Host "Checking for warnings only runs on pull requests."
+        return
+    }
+
+    if (-not $settings.failOnNewWarnings)
+    {
+        Write-Host "Checking for new warnings is not enabled in the settings."
+        return
+    }
+
+    Import-Module (Join-Path $PSScriptRoot "..\Github-Helper.psm1" -Resolve) -DisableNameChecking
+    Import-Module (Join-Path $PSScriptRoot ".\CheckForWarningsUtils.psm1" -Resolve) -DisableNameChecking
+
+    Write-Host "Downloading build logs from previous good build."
+
+    $mask = GetArtifactMask -buildOutputFile $prBuildOutputFile -buildMode $buildMode
+    $projectName = GetProjectName -project $project
+
     Write-Host "Downloading build output for project '$projectName' with mask '$mask'."
     $artifact = GetArtifactsFromWorkflowRun -workflowRun $baselineWorkflowRunId -token $token -api_url $env:GITHUB_API_URL -repository $env:GITHUB_REPOSITORY -mask $mask -projects $projectName | Select-Object -First 1
+
+    if (-not $artifact)
+    {
+        Write-Host "No artifacts found for project '$projectName', skipping check for new warnings."
+        return
+    }
 
     $artifactsFolder = Join-Path $ENV:RUNNER_TEMP ".warnings"
     Initialize-Directory -Path $artifactsFolder
