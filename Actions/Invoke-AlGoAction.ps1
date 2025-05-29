2a3c9efbfe806757d6ed6085da1d81c24768670f
aholstrup1 <aholstrup@microsoft.com>
2025-05-29 09:30:10 +0200

    Log duration of action

--- Actions/Invoke-AlGoAction.ps1
+++ Actions/Invoke-AlGoAction.ps1
@@ -13,16 +13,20 @@ Set-StrictMode -Version 2.0
 
 Import-Module (Join-Path -Path $PSScriptRoot -ChildPath "TelemetryHelper.psm1" -Resolve)
 
+$AdditionalData = @{}
 try {
+    $startTime = Get-Date
     Invoke-Command -ScriptBlock $Action
 
     if (-not $SkipTelemetry) {
-        Trace-Information -ActionName $ActionName
+        $AdditionalData["ActionDuration"] = ((Get-Date) - $startTime).TotalSeconds
+        Trace-Information -ActionName $ActionName -AdditionalData $AdditionalData
     }
 }
 catch {
     if (-not $SkipTelemetry) {
-        Trace-Exception -ActionName $ActionName -ErrorRecord $_
+        $AdditionalData["ActionDuration"] = ((Get-Date) - $startTime).TotalSeconds
+        Trace-Exception -ActionName $ActionName -ErrorRecord $_ -AdditionalData $AdditionalData
     }
 
     Write-Host "::ERROR::Unexpected error when running action. Error Message: $($_.Exception.Message.Replace("`r",'').Replace("`n",' ')), StackTrace: $($_.ScriptStackTrace.Replace("`r",'').Replace("`n",' <- '))";
