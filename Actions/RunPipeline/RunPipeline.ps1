f2cf24d429cbc3cca2cf37552eda154004ab29a6
Alexander Holstrup <117829001+aholstrup1@users.noreply.github.com>
2025-06-12 17:39:07 +0200

    Update RunPipeline.ps1

--- Actions/RunPipeline/RunPipeline.ps1
+++ Actions/RunPipeline/RunPipeline.ps1
@@ -246,6 +246,8 @@ try {
         Write-Host "Analyzing app file $appFile"
         $packageStream = [System.IO.File]::OpenRead($appFile)
         $package = [Microsoft.Dynamics.Nav.CodeAnalysis.Packaging.NavAppPackageReader]::Create($PackageStream, $true)
+        Write-Host "IsRuntimePackage: $($package.IsRuntimePackage)"
+        Write-Host "SourceCodeFilePaths: $($package.ReadSourceCodeFilePaths())"
         if (($null -eq $package.ReadSourceCodeFilePaths()) -and (-not $package.IsRuntimePackage)) {
             # This is likely a symbols package. Write a waning that this app shouldn't be published to a container
             OutputWarning -message "App $app is a symbols package and should not be published to a container. Skipping."
