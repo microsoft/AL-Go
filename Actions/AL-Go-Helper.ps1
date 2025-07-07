eb205b35345cdf49e39bbb745b85c22a5778ea04
Sebastian Petersen <spetersen@microsoft.com>
2025-07-07 16:35:38 +0200

    pre-commit

--- Actions/AL-Go-Helper.ps1
+++ Actions/AL-Go-Helper.ps1
@@ -804,7 +804,7 @@ function ReadSettings {
         else {
             $sampleJson = "{`n`t`"DeployTo$environmentName`": {`n`t`t...`n`t}`n}"
             Write-Host "::warning::The environment settings variable does not contain the property 'DeployTo$environmentName'. Did you define your environment settings at the top JSON level? `nDeployment settings should be defined within the 'DeployTo$environmentName' property, as in other settings like: `n$sampleJson"
-        }    
+        }
         $settingsObjects += @($environmentVariableObject)
     }
 
