c245bb14451539f2c7a3137e9dd58c0594a5073d
Maria Zhelezova <43066499+mazhelez@users.noreply.github.com>
2025-07-09 10:04:26 +0200

    Separate logic related to reading settings (#1743)
    _**Issue**_: AL-Go-Helper has grown significantly, and it currently
    isn't possible to emit telemetry from any the functions in AL-Go-Helper
    
    _**Solution**_: Separate the logic for reading settings into a module. 
    Follow-up will be to leverage the telemetry in key functions in
    AL-Go-Helper.
    
    E2E test run:
    https://github.com/microsoft/AL-Go/actions/runs/16023887227

--- Templates/AppSource App/.AL-Go/localDevEnv.ps1
+++ Templates/AppSource App/.AL-Go/localDevEnv.ps1
@@ -56,11 +56,13 @@ Write-Host -ForegroundColor Yellow @'
 $tmpFolder = Join-Path ([System.IO.Path]::GetTempPath()) "$([Guid]::NewGuid().ToString())"
 New-Item -Path $tmpFolder -ItemType Directory -Force | Out-Null
 $GitHubHelperPath = DownloadHelperFile -url 'https://raw.githubusercontent.com/microsoft/AL-Go-Actions/main/Github-Helper.psm1' -folder $tmpFolder -notifyAuthenticatedAttempt
+$ReadSettingsModule = DownloadHelperFile -url 'https://raw.githubusercontent.com/microsoft/AL-Go-Actions/main/.Modules/ReadSettings.psm1' -folder $tmpFolder
 $ALGoHelperPath = DownloadHelperFile -url 'https://raw.githubusercontent.com/microsoft/AL-Go-Actions/main/AL-Go-Helper.ps1' -folder $tmpFolder
 DownloadHelperFile -url 'https://raw.githubusercontent.com/microsoft/AL-Go-Actions/main/settings.schema.json' -folder $tmpFolder | Out-Null
 DownloadHelperFile -url 'https://raw.githubusercontent.com/microsoft/AL-Go-Actions/main/Packages.json' -folder $tmpFolder | Out-Null
 
 Import-Module $GitHubHelperPath
+Import-Module $ReadSettingsModule
 . $ALGoHelperPath -local
 
 $baseFolder = GetBaseFolder -folder $PSScriptRoot
