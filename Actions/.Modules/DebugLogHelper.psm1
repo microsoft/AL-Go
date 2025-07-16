0e080a327f8fb62ad251fc22e01969f1a35ad78d
spetersenms <79980472+spetersenms@users.noreply.github.com>
2025-07-16 13:33:36 +0200

    Extended debug logging (#1729)

---
+++ Actions/.Modules/DebugLogHelper.psm1
@@ -0,0 +1,218 @@
+
+if ($env:GITHUB_ACTIONS -eq "true") {
+    $runningLocal = $false
+} else {
+    $runningLocal = $true
+}
+
+# Colors
+$colorCodeRed = '31'
+$colorCodeGreen = '32'
+$colorCodeYellow = '33'
+$colorCodeBlue = '34'
+$colorCodeMagenta = '35'
+$colorCodeCyan = '36'
+
+<#
+    .SYNOPSIS
+        Writes debug information about the function call and its parameters if extended debug logging is enabled or running locally.
+    .DESCRIPTION
+        Writes debug information about the function call and its parameters to the console if extended debug logging is enabled or running locally.
+        Automatically retrieves the caller's name and arguments from the call stack.
+#>
+function OutputDebugFunctionCall {
+    try {
+        $caller = (Get-PSCallStack)[1]
+        $callerName = $caller.Command
+        $callerParameters = $caller.InvocationInfo.BoundParameters
+
+        OutputDebug "Function '$callerName' called with parameters:"
+        if ($callerParameters.Count -eq 0) {
+            OutputDebug "None"
+        }
+        foreach ($key in $callerParameters.Keys) {
+            $val = $callerParameters[$key]
+            OutputDebug "-$($key): $val"
+        }
+    } catch {
+        OutputDebug "Unable to retrieve function information from call stack."
+    }
+}
+
+<#
+    .SYNOPSIS
+        Starts a console log group.
+    .DESCRIPTION
+        Starts a console log group. All subsequent log messages will be grouped under this message until OutputGroupEnd is called.
+        If running locally, it writes a simple message to the console. If running in GitHub Actions, it uses the `::group::` command to create a collapsible group in the log.
+    .PARAMETER Message
+        Name/Title of the group.
+#>
+function OutputGroupStart {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string] $Message
+    )
+
+    if ($runningLocal) {
+        Write-Host "==== Group start: $Message ===="
+    } else {
+        Write-Host "::group::$Message"
+    }
+}
+
+<#
+    .SYNOPSIS
+        Ends a console log group.
+    .DESCRIPTION
+        Ends a console log group started with OutputGroupStart. All subsequent log messages will be outside of this group.
+#>
+function OutputGroupEnd {
+    if ($runningLocal) {
+        Write-Host "==== Group end ===="
+    } else {
+        Write-Host "::endgroup::"
+    }
+}
+
+<#
+    .SYNOPSIS
+        Writes to console with optional color.
+    .DESCRIPTION
+        Writes a message to the console with an optional color. If no color is specified, the message is written in the default console color.
+    .PARAMETER Message
+        Message to be written to console.
+    .PARAMETER Color
+        Optional color for the message. Valid values are 'Red', 'Green', 'Yellow', 'Blue', 'Magenta', 'Cyan'.
+#>
+function OutputColor {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string] $Message,
+        [Parameter(Mandatory = $false)]
+        [ValidateSet('Red', 'Green', 'Yellow', 'Blue', 'Magenta', 'Cyan')]
+        [string] $Color
+    )
+
+    if ($Color) {
+        $colorCode = 0
+        switch ($Color) {
+            'Red' { $colorCode = $colorCodeRed }
+            'Green' { $colorCode = $colorCodeGreen }
+            'Yellow' { $colorCode = $colorCodeYellow }
+            'Blue' { $colorCode = $colorCodeBlue }
+            'Magenta' { $colorCode = $colorCodeMagenta }
+            'Cyan' { $colorCode = $colorCodeCyan }
+        }
+        # char 27 is the escape character for ANSI codes which works in both PS 5 and 7.
+        Write-Host "$([char] 27)[${colorCode}m$Message$([char] 27)[0m"
+    } else {
+        Write-Host $Message
+    }
+}
+
+<#
+    .SYNOPSIS
+        Write an error message to the console.
+    .DESCRIPTION
+        Writes an error message to the console. Throws an exception if running locally, otherwise formats the message for GitHub Actions.
+    .PARAMETER Message
+        Message to be written to console.
+#>
+function OutputError {
+    Param(
+        [string] $message
+    )
+
+    if ($runningLocal) {
+        throw $message
+    }
+    else {
+        Write-Host "::Error::$($message.Replace("`r",'').Replace("`n",' '))"
+        $host.SetShouldExit(1)
+    }
+}
+
+<#
+    .SYNOPSIS
+        Write a warning message to the console.
+    .DESCRIPTION
+        Writes a warning message to the console. Uses Write-Warning if running locally, otherwise formats the message for GitHub Actions.
+    .PARAMETER Message
+        Message to be written to console.
+#>
+function OutputWarning {
+    Param(
+        [string] $message
+    )
+
+    if ($runningLocal) {
+        Write-Warning $message
+    }
+    else {
+        Write-Host "::Warning::$message"
+    }
+}
+
+<#
+    .SYNOPSIS
+        Write a notice message to the console.
+    .DESCRIPTION
+        Writes a notice message to the console. Uses regular Write-Host if running locally, otherwise formats the message for GitHub Actions.
+    .PARAMETER Message
+        Message to be written to console.
+#>
+function OutputNotice {
+    Param(
+        [string] $message
+    )
+
+    if ($runningLocal) {
+        Write-Host $message
+    }
+    else {
+        Write-Host "::Notice::$message"
+    }
+}
+
+<#
+    .SYNOPSIS
+        Mask a value in the log.
+    .DESCRIPTION
+        Masks a value in the log to prevent sensitive information from being displayed. If running locally, it writes the masked value to the console.
+    .PARAMETER Value
+        The value to be masked in the log.
+#>
+function MaskValueInLog {
+    Param(
+        [string] $value
+    )
+
+    if (!$runningLocal) {
+        Write-Host "`r::add-mask::$value"
+    }
+}
+
+<#
+    .SYNOPSIS
+        Write a debug message to the console.
+    .DESCRIPTION
+        Writes a debug message to the console. Uses Write-Debug if running locally, otherwise formats the message for GitHub Actions.
+    .PARAMETER Message
+        Message to be written to console.
+#>
+function OutputDebug {
+    Param(
+        [string] $message
+    )
+
+    if ($runningLocal) {
+        Write-Debug $message
+    }
+    else {
+        Write-Host "::Debug::[AL-Go]$message"
+    }
+}
+
+Export-ModuleMember -Function OutputColor, OutputDebugFunctionCall, OutputGroupStart, OutputGroupEnd, OutputError, OutputWarning, OutputNotice, MaskValueInLog, OutputDebug
+Export-ModuleMember -Variable debugLoggingEnabled
--- Actions/AL-Go-Helper.ps1
+++ Actions/AL-Go-Helper.ps1
@@ -4,6 +4,7 @@ Param(
 
 $gitHubHelperPath = Join-Path $PSScriptRoot 'Github-Helper.psm1'
 $readSettingsModule = Join-Path $PSScriptRoot '.Modules/ReadSettings.psm1'
+$debugLoggingModule = Join-Path $PSScriptRoot '.Modules/DebugLogHelper.psm1'
 if (Test-Path $gitHubHelperPath) {
     Import-Module $gitHubHelperPath
     # If we are adding more dependencies here, then localDevEnv and cloudDevEnv needs to be updated
@@ -11,6 +12,10 @@ if (Test-Path $gitHubHelperPath) {
 
 if (Test-Path $readSettingsModule) {
     Import-Module $readSettingsModule
+}
+
+if (Test-Path $debugLoggingModule) {
+    Import-Module $debugLoggingModule
 }
 
 $errorActionPreference = "Stop"; $ProgressPreference = "SilentlyContinue"; Set-StrictMode -Version 2.0
@@ -22,7 +27,6 @@ $RepoSettingsFile = Join-Path '.github' 'AL-Go-Settings.json'
 $defaultCICDPushBranches = @( 'main', 'release/*', 'feature/*' )
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', 'defaultCICDPullRequestBranches', Justification = 'False positive.')]
 $defaultCICDPullRequestBranches = @( 'main' )
-$runningLocal = $local.IsPresent
 $defaultBcContainerHelperVersion = "preview" # Must be double quotes. Will be replaced by BcContainerHelperVersion if necessary in the deploy step - ex. "https://github.com/organization/navcontainerhelper/archive/refs/heads/branch.zip"
 $notSecretProperties = @("Scopes","TenantId","BlobName","ContainerName","StorageAccountName","ServerUrl","ppUserName","GitHubAppClientId","EnvironmentName")
 
@@ -180,69 +184,6 @@ function ConvertTo-HashTable() {
             }
         }
         $ht
-    }
-}
-
-function OutputError {
-    Param(
-        [string] $message
-    )
-
-    if ($runningLocal) {
-        throw $message
-    }
-    else {
-        Write-Host "::Error::$($message.Replace("`r",'').Replace("`n",' '))"
-        $host.SetShouldExit(1)
-    }
-}
-
-function OutputWarning {
-    Param(
-        [string] $message
-    )
-
-    if ($runningLocal) {
-        Write-Host -ForegroundColor Yellow "WARNING: $message"
-    }
-    else {
-        Write-Host "::Warning::$message"
-    }
-}
-
-function OutputNotice {
-    Param(
-        [string] $message
-    )
-
-    if ($runningLocal) {
-        Write-Host $message
-    }
-    else {
-        Write-Host "::Notice::$message"
-    }
-}
-
-function MaskValueInLog {
-    Param(
-        [string] $value
-    )
-
-    if (!$runningLocal) {
-        Write-Host "`r::add-mask::$value"
-    }
-}
-
-function OutputDebug {
-    Param(
-        [string] $message
-    )
-
-    if ($runningLocal) {
-        Write-Host $message
-    }
-    else {
-        Write-Host "::Debug::$message"
     }
 }
 
