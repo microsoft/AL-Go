d115f02a85bc76df2e16ce924a0f69472eacf56c
Alexander Holstrup <117829001+aholstrup1@users.noreply.github.com>
2025-08-27 11:45:17 +0000

    Santize workflows

--- Actions/.Modules/ReadSettings.psm1
+++ Actions/.Modules/ReadSettings.psm1
@@ -394,8 +394,9 @@ function ReadSettings {
 
                                 # If propname is workflows then we should sanitize the workflow name in the same way we sanitize the $workflowName variable
                                 Write-Host "1: $propName = $propName, propValue = $propValue"
-                                if(($propName -eq "workflows") -and ($null -ne $propValue)) {
-                                    $propValue = SanitizeWorkflowName -workflowName $propValue
+                                if($propName -eq "workflows") {
+                                    Write-Host "Sanitizing workflow names in conditional settings"
+                                    $conditionalSetting."$propName" = $conditionalSetting."$propName" | ForEach-Object { SanitizeWorkflowName -workflowName $_ }
                                 }
                                 Write-Host "2: $propName = $propName, propValue = $propValue"
 
