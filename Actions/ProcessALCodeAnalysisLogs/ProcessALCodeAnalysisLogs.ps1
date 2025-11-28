1e777204f43923e73197c0970ee49f09ef19b89e
spetersenms <spetersenms@users.noreply.github.com>
2025-11-28 10:15:29 +0100

    Grammar fix

--- Actions/ProcessALCodeAnalysisLogs/ProcessALCodeAnalysisLogs.ps1
+++ Actions/ProcessALCodeAnalysisLogs/ProcessALCodeAnalysisLogs.ps1
@@ -78,7 +78,7 @@ function GenerateSARIFJson {
                 $fullMessage = "$($issue.ruleId): $fullMessage"
             }
 
-            # Use only full message for rules if possible. The messages from the al compiler looks like this:
+            # Use only full message for rules if possible. The messages from the AL compiler look like this:
             # "shortMessage": "Variable 'InvalidDate' is unused in 'CustomerListExtTwo'.",
             # "fullMessage": "Do not declare variables that are unused."
             # So if shortMessage is used, the rule description will not be generic, but specific to a certain alert result.
