88031aef4e015a83a3597397f2a8b908b7b69954
Freddy Kristiansen <fkr@freddy.dk>
2026-01-15 06:28:24 +0100

    use trim

--- Actions/Github-Helper.psm1
+++ Actions/Github-Helper.psm1
@@ -958,7 +958,7 @@ function FindLatestSuccessfulCICDRun {
             break
         }
 
-        $CICDRuns = @($workflowRuns.workflow_runs | Where-Object { $_.name -eq ' CI/CD' })
+        $CICDRuns = @($workflowRuns.workflow_runs | Where-Object { $_.name.Trim() -eq 'CI/CD' })
 
         foreach($CICDRun in $CICDRuns) {
             if($CICDRun.conclusion -eq 'success') {
