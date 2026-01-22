97f8c75dc3f1f828a51928bbdef7c1fafb87085d
spetersenms <spetersenms@users.noreply.github.com>
2026-01-22 10:47:34 +0100

    Add input to not create new environment if user input name that does not exist.

--- Actions/DetermineDeploymentEnvironments/DetermineDeploymentEnvironments.ps1
+++ Actions/DetermineDeploymentEnvironments/DetermineDeploymentEnvironments.ps1
@@ -3,7 +3,9 @@
     [string] $getEnvironments,
     [Parameter(HelpMessage = "Type of deployment (CD, Publish or All)", Mandatory = $true)]
     [ValidateSet('CD','Publish','All')]
-    [string] $type
+    [string] $type,
+    [Parameter(HelpMessage = "If true, allow deploying to environments that do not exist", Mandatory = $false)]
+    [switch] $createEnvIfNotExists
 )
 OutputDebugFunctionCall
 
@@ -104,7 +106,12 @@ $unknownEnvironment = 0
 if (!($environments)) {
     # If no environments are defined and the user specified a single environment, use that environment
     # This allows the user to specify a single environment without having to define it in the settings
-    if ($getenvironments -notcontains '*' -and $getenvironments -notcontains '?' -and $getenvironments -notcontains ',') {
+    # Check that the environment name doesn't contain wildcards or comma (which would indicate a pattern)
+    if ($getEnvironments.IndexOf('*') -lt 0 -and $getEnvironments.IndexOf('?') -lt 0 -and $getEnvironments.IndexOf(',') -lt 0) {
+        # Only allow creating unknown environments if explicitly requested
+        if (-not $createEnvIfNotExists) {
+            throw "Environment '$getEnvironments' does not exist in GitHub nor in the AL-Go settings. To create a new GitHub environment for this deployment, run the workflow again with 'Create environment if it does not exist' enabled."
+        }
         $envName = $getEnvironments.Split(' ')[0]
         $deploymentEnvironments += @{
             "$getEnvironments" = @{
