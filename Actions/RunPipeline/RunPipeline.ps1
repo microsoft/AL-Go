c30ee592bc9c243f2da09af306ae3baa021a7eeb
aholstrup1 <aholstrup@microsoft.com>
2026-03-02 10:19:43 +0100

    casing

--- Actions/RunPipeline/RunPipeline.ps1
+++ Actions/RunPipeline/RunPipeline.ps1
@@ -458,7 +458,7 @@ try {
                         try {
                             Publish-BcNuGetPackageToContainer -containerName $parameters.containerName -tenant $parameters.tenant -skipVerification -appSymbolsFolder $parameters.appSymbolsFolder @publishParams
                         } catch {
-                            OutputWarning -message "Failed to publish app $appId version $version to container $($parameters.containerName). Error was: $($_.Exception.Message)."
+                            OutputWarning -message "Failed to publish app $appid version $version to container $($parameters.containerName). Error was: $($_.Exception.Message)."
                         }
                     }
                     else {
